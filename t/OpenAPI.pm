d5a3036082f00da86ca062eceacb472b9ccca707
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2007-11-29 09:48:22 +0000

    r3279@agentz-office (orig r762):  agentzh | 2007-11-29 18:00:44 +0800  [openapi] refactored the test scaffold
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@21 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ t/OpenAPI.pm
@@ -0,0 +1,62 @@
+package t::OpenAPI;
+
+use Test::Base -Base;
+
+#use Smart::Comments;
+use LWP::UserAgent;
+
+our @EXPORT = qw(init run_tests run_test);
+
+my $ua = LWP::UserAgent->new;
+my $host = 'http://localhost';
+
+sub init {
+    my $res = do_request('DELETE', $host . '/=/model');
+    ok $res->is_success, 'DELETE /=/model failed';
+    is $res->content, '', 'no error msg';
+}
+
+#init();
+
+sub do_request ($$$$) {
+    my ($method, $url, $body, $type) = @_;
+    $type ||= 'text/plain';
+    my $req = HTTP::Request->new($method);
+    $req->header('Content-Type' => $type);
+    $req->header('Accept', '*/*');
+    $req->url($url);
+    if ($body) {
+        $req->content($body);
+    }
+    return $ua->request($req);
+}
+
+
+sub run_tests () {
+    for my $block (blocks()) {
+        run_test($block);
+    }
+}
+
+sub run_test ($) {
+    my $block = shift;
+    my $name = $block->name;
+    my $request = $block->request;
+    my $type = $block->request_type;
+    if ($request =~ /^(GET|POST|HEAD|PUT|DELETE)\s+(\S+)\s*\n(.*)/s) {
+        my ($method, $url, $body) = ($1, $2, $3);
+        ### $method
+        ### $url
+        ### $body
+        ### $host
+        my $res = do_request($method, $host.$url, $body, $type);
+        ok $res->is_success, "request returns OK - $name";
+        is $res->content, $block->response, "response content OK - $name";
+    } else {
+        my ($firstline) = ($request =~ /^([^\n]*)/s);
+        die "Invalid request head: \"$firstline\" in $name\n";
+    }
+}
+
+1;
+
--- t/sanity.t
+++ t/sanity.t
@@ -1,52 +1,8 @@
-use Test::Base;
-use LWP::UserAgent;
-#use Smart::Comments;
+use t::OpenAPI;
 
 plan tests => 2 * blocks();
 
-my $ua = LWP::UserAgent->new;
-my $host = 'http://localhost';
-
-sub init {
-    my $res = do_request('DELETE', $host . '/=/model');
-    ok $res->is_success, 'DELETE /=/model failed';
-    is $res->content, '', 'no error msg';
-}
-
-#init();
-
-sub do_request {
-    my ($method, $url, $body, $type) = @_;
-    $type ||= 'text/plain';
-    my $req = HTTP::Request->new($method);
-    $req->header('Content-Type' => $type);
-    $req->header('Accept', '*/*');
-    $req->url($url);
-    if ($body) {
-        $req->content($body);
-    }
-    return $ua->request($req);
-}
-
-run {
-    my $block = shift;
-    my $name = $block->name;
-    my $request = $block->request;
-    my $type = $block->request_type;
-    if ($request =~ /^(GET|POST|HEAD|PUT|DELETE)\s+(\S+)\s*\n(.*)/s) {
-        my ($method, $url, $body) = ($1, $2, $3);
-        ### $method
-        ### $url
-        ### $body
-        my $res = do_request($method, $host.$url, $body, $type);
-        ok $res->is_success, "request returns OK - $name";
-        is $res->content, $block->response, "response content OK - $name";
-    } else {
-        my ($firstline) = ($request =~ /^([^\n]*)/s);
-        die "Invalid request head: \"$firstline\" in $name\n";
-    }
-
-};
+run_tests;
 
 __DATA__
 
