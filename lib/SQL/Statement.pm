e964284d1d6c81c46c1d3c08308c40d72912fdb8
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2007-12-10 09:42:07 +0000

    r3646@agentz-office (orig r946):  agentzh | 2007-12-10 17:56:31 +0800  [openapi] added the missing SQL::Statement class
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@200 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ lib/SQL/Statement.pm
@@ -0,0 +1,25 @@
+package SQL::Statement;
+
+use strict;
+use warnings;
+use base 'Clone';
+use overload '""' => sub { $_[0]->generate };
+
+sub reset {
+    my $self = shift;
+    %$self = %{ $self->new(@_) };
+    $self;
+}
+
+sub where {
+    my $self = shift;
+    if (@_ == 2) {
+        push @{ $self->{where} }, [$_[0], '=', $_[1]];
+    } else {
+        push @{ $self->{where} }, [@_];
+    }
+    $self;
+}
+
+1;
+
