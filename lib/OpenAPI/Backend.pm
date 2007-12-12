1a0d67b8c74180ade86599d4aa8f1b9c525e98d9
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2007-12-12 08:17:23 +0000

    r3670@agentz-office (orig r957):  agentzh | 2007-12-11 14:45:29 +0800  [openapi] added the missing OpenAPI::Backend class
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@212 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ lib/OpenAPI/Backend.pm
@@ -0,0 +1,19 @@
+package OpenAPI::Backend;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $backend) = @_;
+    if (!$backend) {
+        die "No backend specified";
+    }
+    my $backend_class = $class . '::' . $backend;
+    eval "use $backend_class";
+    if ($@) {
+        die $@;
+    }
+    $backend_class->new;
+}
+
+1;
