02d4dd9550ac6174958582022a567d5893c315e0
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2007-12-13 10:46:53 +0000

    r3702@agentz-office (orig r980):  laser | 2007-12-13 15:40:42 +0800  add $user._roles support to Pg.pm and rearranged the create index DDL a little for PgFarm.pm & Pg.pm
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@224 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenAPI/Backend/Pg.pm
+++ lib/OpenAPI/Backend/Pg.pm
@@ -67,11 +67,14 @@ sub add_user {
     my ($self, $user) = @_;
     my $retval = $self->do(<<"_EOC_");
     create schema $user;
+
     create table $user._models (
+	id serial primary key,
         name text primary key,
         table_name text unique,
         description text
     );
+
     create table $user._columns (
         id serial primary key,
         name text,
@@ -80,6 +83,20 @@ sub add_user {
         native_type varchar(20),
         label text
     );
+
+    create table $user._roles(
+	id serial primary key,
+	name text,
+        parentRole integer default 0, -- a column reference to $user._roles itself.
+        password text,
+	obj integer, -- a column reference to $user._columns or $user._models
+        accessable boolean,
+	readable boolean,
+        writeable boolean,
+        manageable boolean
+			      );
+    create unique index $user.$user__models_name_idx on $user._models using btree (name);
+    create unique index $user.$user__roles_name_idx on $user._roles using btree(name);
 _EOC_
     #$retval += 0;
     return $retval;
