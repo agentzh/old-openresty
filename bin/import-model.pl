533fdacaf2e13143be14fceabdaf3c06d050b453
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-09 10:46:22 +0000

    r6905@agentz-office (orig r1427):  agentzh | 2008-01-09 18:43:46 +0800  [openapi] added import-model.pl
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@619 b2c27d59-8999-dc11-853b-000bcdefe56b

--- bin/import-model.pl
+++ bin/import-model.pl
@@ -1,10 +1,5 @@
 #!/usr/bin/env perl
 
-# Sample:
-# bin/export-model.pl --user carrie --model YisouComments \
-#  --server http://ced02.search.cnb.yahoo.com/openapi \
-#  > YisouComments.yml
-
 use strict;
 use warnings;
 
@@ -30,32 +25,43 @@ $user or die "No user given.\n";
 $model or die "No model given.\n";
 $server or die "No server given.\n";
 
+my $yaml = do { local $/; <> };
+my $data = YAML::Syck::Load($yaml);
+_ARRAY0($data) or die "The YAML data is not an array.\n";
+my @rows = @$data;
+
 my $offset = 0;
-my $count = 100;
-my @rows;
+my $count = 20;
+my $url = "$server/=/model/$model/~/~?user=$user";
+if ($password) { $url .= "\&password=$password"; }
+do_request(DELETE => $url, undef, undef);
 while (1) {
+    last if $offset >= $#rows;
     warn "$offset\n";
     my $url = "$server/=/model/$model/~/~?user=$user\&offset=$offset\&count=$count\&order_by=id:asc";
     if ($password) {
         $url .= '&password=' . $password;
     }
-    my $res = do_request(GET => $url, undef, undef);
+    my $to = $offset+$count-1;
+    my @elems = @rows[$offset..($to > $#rows ? $#rows : $to)];
+    #warn "count: ", scalar(@elems), "\n";
+    my $json = JSON::Syck::Dump(\@elems);
+    $json = encode('UTF-8', $json);
+    #warn $json;
+
+    my $res = do_request(POST => $url, $json, undef);
     if (!$res->is_success) {
         die "$url: ", $res->status_line, "\n";
     }
-    my $json = $res->content;
-    $json or die "No content found in server's response.\n";
-    $json = decode('utf8', $json);
-    my $data = JSON::Syck::Load($json);
+    my $res_json = $res->content;
+    $res_json or die "No content found in server's response.\n";
+    #$res_json = decode('UTF-8', $res_json);
+    my $data = JSON::Syck::Load($res_json);
     if (_HASH($data) && !$data->{success} && $data->{error}) {
-        die "Error from the server: $json\n";
-    }
-    elsif (_ARRAY0($data)) {
-        push @rows, @$data;
-        last if @$data < $count;
+        die "Error from the server: $res_json: $json\n";
     }
 } continue { $offset += $count }
 
-warn "For tatal ", scalar(@rows), " records obtained.\n";
-print encode('UTF-8', YAML::Syck::Dump(\@rows));
+warn "For tatal ", scalar(@rows), " records inserted.\n";
+#print encode('UTF-8', YAML::Syck::Dump(\@rows));
 
