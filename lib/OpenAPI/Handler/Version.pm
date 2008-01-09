ced22e87feecf8b0c989687acace9ab6dba5afd1
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-09 08:53:37 +0000

    r6899@agentz-office (orig r1423):  agentzh | 2008-01-09 16:51:01 +0800  [openapi] makes use of $OpenAPI::VERSION in the Version handler
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@617 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenAPI/Handler/Version.pm
+++ lib/OpenAPI/Handler/Version.pm
@@ -11,7 +11,7 @@ use vars qw($VERSION $Revision);
 sub GET_version {
     my ($self, $bits) = @_;
     $Revision ||= slurp("$FindBin::Bin/../revision") || 'Unknown';
-    return "OpenAPI 1.0.0 (revision $Revision)\nCopyright (c) 2007-2008 Yahoo! China EEEE\n";
+    return "OpenAPI $VERSION (revision $Revision)\nCopyright (c) 2007-2008 Yahoo! China EEEE\n";
 }
 
 1;
