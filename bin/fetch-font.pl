d06459b6dc5c40b968df50a1e1f1b59d2d63c5ff
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-11 02:20:20 +0000

    r6920@agentz-office (orig r1435):  agentzh | 2008-01-11 10:16:35 +0800  [openapi] now we fetch ttf in Makefile.PL
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@627 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ bin/fetch-font.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+
+my $url = 'http://agentzh.org/misc/wqy-zenhei.ttf';
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+$ua->env_proxy;
+
+my $file = 'font/wqy-zenhei.ttf';
+unless (-f $file and -s $file > 13000000) {
+    warn "Fetching $url...\n";
+    $ua->mirror($url, $file);
+}
+
