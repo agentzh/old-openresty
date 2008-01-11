b6dd11d810f2cfede7f8a0d25a35e14623b39e88
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-11 02:19:41 +0000

    r6919@agentz-office (orig r1434):  agentzh | 2008-01-10 20:17:35 +0800  [openapi] updated openapi.js
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@626 b2c27d59-8999-dc11-853b-000bcdefe56b

--- clients/openapi.js
+++ clients/openapi.js
@@ -1,38 +1,43 @@
-OpenAPI = function (params) {
+if (typeof window.OpenAPI == "undefined") {
+
+window.undefined = window.undefined;
+
+var OpenAPI = function (params) {
     if (params == undefined) params = {};
-    if (params.server) {
-        self.server = params.server;
-        self.callback = params.callback;
+    this.callback = params.callback;
+    this.server = params.server;
+    if (!this.server) throw "No server specified for OpenAPI.new";
+    if (!this.callback) throw "No callback specified for OpenAPI.new";
+};
+
+OpenAPI.prototype.get = function (url, args) {
+    if (!args) args = {};
+    if (url.match(/\?/)) throw "URL should not contain '?'.";
+    args.rand = Math.random();
+    args.callback = this.callback;
+    var scriptTag = document.createElement("script");
+    scriptTag.id = "openapiScriptTag";
+    scriptTag.className = 'openapiScriptTag';
+    var arg_list = new Array();
+    for (var key in args) {
+        arg_list.push(key + "=" + encodeURIComponent(args[key]));
+    }
+    scriptTag.src = this.server + url + "?" + arg_list.join("&");
+    scriptTag.type = "text/javascript";
+    var headTag = document.getElementsByTagName('head')[0];
+    headTag.appendChild(scriptTag);
+};
+
+OpenAPI.purge = function () {
+    // document.getElementByClassName('openapiScriptTag').remove();
+    var nodes = document.getElementsByTagName('script');
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.className == 'openapiScriptTag') {
+            node.removeNode(false);
+        }
     }
 };
 
-OpenAPI.prototype = {
-    get: function (url, args) {
-        if (!args) args = {};
-        if (url.match(/\?/)) throw "URL should not contain '?'";
-        args.rand = Math.random();
-        args.callback = this.callback;
-        var scriptTag = document.createElement("script");
-	scriptTag.id = "openapiScriptTag" + args.rand;
-        scriptTag.className = 'openapiScriptTag';
-        var arg_list = new Array();
-        for (var key in args) {
-            arg_list.push(key + "=" + encodeURIComponent(args[key]));
-        }
-	scriptTag.src = url + "?" + arg_list.join("&");
-	scriptTag.type = "text/javascript";
-	var headTag = document.getElementsByTagName('head')[0];
-	headTag.appendChild(scriptTag);
-    },
-    purge: function () {
-        // document.getElementByClassName('openapiScriptTag').remove();
-        var nodes = document.getElementsByTagName('script');
-        for (var i = 0; i < nodes.length; i++) {
-            var node = nodes[i];
-            if (node.className == 'openapiScriptTag') {
-                node.removeNode(false);
-            }
-        }
-    },
-};
+}
 
