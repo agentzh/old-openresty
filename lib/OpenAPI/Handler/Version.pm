c1547e949d936fefa3050f4dcc6124a8486a4183
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-15 08:31:06 +0000

    r6980@agentz-office (orig r1472):  agentzh | 2008-01-15 16:28:03 +0800  [openapi] made /=/version more informative and also fixed the issue reported by laser
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@644 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenAPI/Handler/Version.pm
+++ lib/OpenAPI/Handler/Version.pm
@@ -11,7 +11,15 @@ use vars qw($VERSION $Revision);
 sub GET_version {
     my ($self, $bits) = @_;
     $Revision ||= slurp("$FindBin::Bin/../revision") || 'Unknown';
-    return "OpenAPI $VERSION (revision $Revision)\nCopyright (c) 2007-2008 Yahoo! China EEEE\n";
+    my $backend = $OpenAPI::BackendName;
+    if ($backend eq 'PgFarm') {
+        my $dns = $OpenAPI::Backend::PgFarm::DNS;
+        if ($dns =~ /host=([^=\s]+)/) {
+            $dns = $1;
+        }
+        $backend .= " ($1)";
+    }
+    return "OpenAPI $VERSION (revision $Revision) with the $backend backend.\nCopyright (c) 2007-2008 Yahoo! China EEEE\n";
 }
 
 1;
