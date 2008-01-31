88ea832a0ed4cbf517e877307c0eb53f4a269175
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-31 10:43:31 +0000

    r7365@agentz-office (orig r1711):  agentzh | 2008-01-31 18:21:33 +0800  [openapi] fixed the login method in WWW::OpenAPI
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@760 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/WWW/OpenAPI/Simple.pm
+++ lib/WWW/OpenAPI/Simple.pm
@@ -3,19 +3,25 @@ package WWW::OpenAPI::Simple;
 use strict;
 use warnings;
 
-use Carp 'confess';
+#use Carp 'confess';
 use JSON::Syck ();
 use base 'WWW::OpenAPI';
+use Params::Util qw( _HASH );
 
 sub request {
     my $self = shift;
+    my $meth = $_[1];
+    my $url = $_[2];
     my $res = $self->SUPER::request(@_);
     if ($res->is_success) {
         my $json = $res->content;
         my $data = JSON::Syck::Load($json);
+        if (_HASH($data) && !$data->{success}) {
+            die "$meth $url: $json";
+        }
         return $data;
     }
-    confess "$_[2] $_[3]: ", $res->status_line;
+    die "$meth $url: ", $res->status_line;
 }
 
 1;
