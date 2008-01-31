6b85a94f5d22d5bfe15c6c850b2578d91a3e8d2f
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-01-31 03:37:27 +0000

    r7301@agentz-office (orig r1675):  leiyh | 2008-01-30 19:12:32 +0800  add return ture forever
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@745 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenAPI/Backend/Pg.pm
+++ lib/OpenAPI/Backend/Pg.pm
@@ -120,7 +120,7 @@ sub login {
 
     $retval = $self->do(<<"_EOC_");
 create or replace function login(account text, role text,
-  captcha text, pass text) as \$\$
+  captcha text, pass text) returns integer as \$\$
 declare
   u text;
 begin
@@ -147,6 +147,7 @@ begin
       raise exception 'Password for %.% is required.', account , role;
     end if;
   end if;
+  return 0;
 end;
 \$\$ language plpgsql;
 select login($account, $role, $captcha, $pass);
