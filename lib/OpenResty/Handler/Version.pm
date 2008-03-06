166a9d856c7a81e80c74c63cad5cf7a5685fae59
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-03-06 06:24:02 +0000

    r7870@agentz-office (orig r1962):  agentzh | 2008-03-06 14:18:58 +0800  [openresty] fixed Version.pm
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@970 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty/Handler/Version.pm
+++ lib/OpenResty/Handler/Version.pm
@@ -30,7 +30,7 @@ sub GET_version {
         }
         $backend .= " ($host)";
     }
-    (my $ver = $OpenResty::VERSION) =~ s/(\d+)\.0*(\d+).0*(\d+)/$1\.$2\.$3/;
+    (my $ver = $OpenResty::VERSION) =~ s{^(\d+)\.(\d{3})(\d{3})?$}{join '.', int($1), int($2), int($3||0)}e;
     return "OpenResty $ver (revision $Revision) with the $backend backend.\nCopyright (c) 2007-2008 by Yahoo! China EEEE Works, Alibaba Inc.\n";
 }
 
