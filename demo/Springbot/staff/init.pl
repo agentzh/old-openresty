10bccdd25ce8e793ce4e8aaa866e209d1f58f03a
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-03-26 10:04:49 +0000

    r8209@agentz-office (orig r2106):  agentzh | 2008-03-26 18:00:14 +0800  [openresty] set mime-type for springbot and init.pl
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1049 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ demo/Springbot/staff/init.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Params::Util qw(_HASH);
+use WWW::OpenResty::Simple;
+use JSON::XS;
+use Getopt::Std;
+
+my %opts;
+getopts('u:p:', \%opts) or
+    die "Usage: ./init.pl -u <user> -p <password>\n";
+
+my $user = $opts{u} or die "No -u specified.\n";
+my $password = $opts{p} or die "No -p specified.\n";
+
+my $resty = WWW::OpenResty::Simple->new(
+    { server => 'resty.eeeeworks.org' }
+);
+$resty->login($user, $password);
+if (!has_model($resty, 'YahooStaff')) {
+    $resty->post(
+        '/=/model/YahooStaff',
+        {
+            description => 'Yahoo! China Staff',
+            columns => [
+                { name => 'name', label => 'Name' },
+                { name => 'employee_id', label => 'Employee Identifier' },
+                { name => 'department', label => 'Department Name' },
+                { name => 'email', label => 'Email address' },
+                { name => 'office_phone', label => 'Office phone number' },
+                { name => 'cellphone', label => 'Cellphone number' },
+                { name => 'yahoo_im_id', label => 'Yahoo! Instant Messager ID' },
+            ],
+        }
+    );
+}
+
+my $res = $resty->get('/=/model');
+print dumper($res);
+
+sub dumper {
+    JSON::XS->new->utf8->pretty->encode($_[0]);
+}
+
+sub has_model {
+    my ($resty, $model) = @_;
+    eval {
+        $resty->get("/=/model/$model");
+    };
+    if ($@ && $@ =~ /Model .*? not found/i) {
+        return undef;
+    }
+    return 1;
+}
+
