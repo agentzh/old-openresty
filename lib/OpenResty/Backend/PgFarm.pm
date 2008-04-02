735b4a947bdf551ba8ff7aa1fc14be3c1d5a0278
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-04-02 06:23:11 +0000

    r8269@agentz-office (orig r2139):  agentzh | 2008-04-02 14:18:09 +0800  [openresty] added --retries option to import-model.pl
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1073 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty/Backend/PgFarm.pm
+++ lib/OpenResty/Backend/PgFarm.pm
@@ -4,11 +4,14 @@ use strict;
 use warnings;
 
 #use Smart::Comments;
-use JSON::XS;
+use JSON::XS ();
 use DBI;
+use Encode ();
 use base 'OpenResty::Backend::Base';
 
 our ($Host, $User, $Password, $Port);
+
+my $json_xs = JSON::XS->new->utf8;
 
 sub new {
     #
@@ -49,10 +52,11 @@ sub select {
     ### JSON: $res->[0][0]
     my $json = $res->[0][0];
     eval {
-        $res = decode_json($json);
+        $res = $json_xs->decode($json);
     };
     if ($@) {
-        die "Failed to load JSON from PgFarm's response: $@\n$json";
+        use Data::Dumper;
+        die "Failed to load JSON from PgFarm's response: $@\n", Data::Dumper::Dumper($json);
     }
     return $res;
 }
