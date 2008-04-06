d1c14e3a966b4332619df4f318235393aee7e33c
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-04-06 12:30:31 +0000

    r8373@agentz-office (orig r2191):  agentzh | 2008-04-06 20:25:29 +0800  demo/Admin - the login.html page is mostly functional :)
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1120 b2c27d59-8999-dc11-853b-000bcdefe56b

--- demo/Admin/js/login.js
+++ demo/Admin/js/login.js
@@ -1,3 +1,6 @@
+var cookieName = 'admin_session';
+var openresty = null;
+
 $(document).ready(init);
 
 function init () {
@@ -5,5 +8,44 @@ function init () {
     $("#register-link").click( function () {
         alert('For now, please write to "agentzh" <agentzh@yahoo.cn> to request one :)');
     } );
+    $("#login-button").click(login);
+}
+
+function error (msg) {
+    alert(msg);
+}
+
+function debug (msg) {
+    $("#main").append(msg + "<br/>");
+}
+
+
+function login () {
+    var server = $("#login-server").val();
+    var user = $("#login-user").val();
+    var password = $("#login-password").val();
+    if (!server) {
+        error("No server specified.");
+        return false;
+    }
+    if (!user) {
+        error("No user specified.");
+        return false;
+    }
+    openresty = new OpenResty.Client( { server: server } );
+    openresty.callback = afterLogin;
+    openresty.login(user, password);
+    return false;
+}
+
+function afterLogin (res) {
+    //alert("After login!");
+    if (!openresty.isSuccess(res)) {
+        error("Failed to login: " + res.error);
+        return;
+    }
+    $.cookie(cookieName, res.session, { path: '/', expires: 2 /* days */ });
+    //alert("saved cookie: " + $.cookie(cookieName));
+    location = 'index.html';
 }
 
