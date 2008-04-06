977255f6d813914ca6a703ad5ad946bbdadf2990
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-04-06 11:39:01 +0000

    r8371@agentz-office (orig r2190):  agentzh | 2008-04-06 19:34:01 +0800  demo/Admin - moved JS from login.html to login.js
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1119 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ demo/Admin/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(init);
+
+function init () {
+    //alert("HERE!");
+    $("#register-link").click( function () {
+        alert('For now, please write to "agentzh" <agentzh@yahoo.cn> to request one :)');
+    } );
+}
+
