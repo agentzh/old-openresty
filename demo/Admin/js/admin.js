e55aecccc7f73c3fffd572ccfa706e3cc854029d
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-04-07 06:08:53 +0000

    r8393@agentz-office (orig r2201):  agentzh | 2008-04-07 14:03:38 +0800  demo/Admin - implement the view for individual models, views, and roles
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1130 b2c27d59-8999-dc11-853b-000bcdefe56b

--- demo/Admin/js/admin.js
+++ demo/Admin/js/admin.js
@@ -7,10 +7,6 @@ var loadingCount = 0;
 var waitMessage = null;
 var savedAnchor = null;
 
-var modelList = null;
-var viewList = null;
-var roleList = null;
-
 $(document).ready(init);
 
 function error (msg) {
@@ -33,7 +29,6 @@ function init () {
     if (!server || !session) {
         location = 'login.html';
     }
-    $("#logout-link").click(removeCookies);
 
     waitMessage = document.getElementById('wait-message');
 
@@ -44,34 +39,4 @@ function init () {
     setInterval(dispatchByAnchor, 600);
 }
 
-function dispatchByAnchor () {
-    var anchor = location.hash;
-    anchor = anchor.replace(/^\#/, '');
-    if (savedAnchor == anchor)
-        return;
-    if (anchor == "") {
-        anchor = 'models';
-        location.hash = 'models';
-    }
-    savedAnchor = anchor;
-
-    // prevent memory leaks from dynamically created <script> nodes:
-    if (loadingCount <= 0) openresty.purge();
-    loadingCount = 0;
-
-    if (anchor == 'models') {
-        getModels( { cache: false } );
-        return;
-    }
-    if (anchor == 'views') {
-        getViews( { cache: false } );
-        return;
-    }
-    if (anchor == 'roles') {
-        getRoles( { cache: false } );
-        return;
-    }
-
-    alert("Not implemented :(");
-}
 
