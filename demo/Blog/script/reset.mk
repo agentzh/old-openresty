8caae6c9388822a5afa320511832793471853b25
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-05 08:58:16 +0000

    r8852@agentz-office:  agentz | 2008-05-05 16:53:29 +0800  added Blog/script/reset.mk and removed Blog2/script/init.pl as per chuanwen++
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1299 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ demo/Blog/script/reset.mk
@@ -0,0 +1,15 @@
+password=
+server=
+user=
+
+export:
+	-mkdir backup
+	-cp *.json backup
+	../../bin/export-model.pl --user $(user) --password $(password) --model Comment --server $(server) --out Comment.json
+	../../bin/export-model.pl --user $(user) --password $(password) --model Post --server $(server) --out Post.json
+
+import:
+	script/init.pl -u $(user) -p $(password) -s $(server)
+	../../bin/import-model.pl --reset --step 2 --no-id --user $(user) --password $(password) --model Post --server $(server) Post.json
+	../../bin/import-model.pl --reset --no-id --user $(user) --password $(password) --model Comment --server $(server) Comment.json
+
