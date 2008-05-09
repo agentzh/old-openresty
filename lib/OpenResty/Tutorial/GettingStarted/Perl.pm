8cef91c12cf8ff727990c9b307b5f46bf25de2fa
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-09 10:26:31 +0000

    r8934@agentz-office:  agentz | 2008-05-09 18:19:58 +0800  added the backbone for OpenResty::Tutorial::GettingStarted::Perl
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1323 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ lib/OpenResty/Tutorial/GettingStarted/Perl.pm
@@ -0,0 +1,36 @@
+=encoding utf8
+
+=head1 NAME
+
+OpenResty::Tutorial::GettingStarted - Zero to OpenResty for Perl programmers
+
+=head1 DESCRIPTION
+
+This tutorial should give you everything you need to start with an OpenResty account using Perl.
+
+=head1 Prerequisites
+
+You should already have an account on an OpenResty server. You can either set up an OpenResty server on your own machine or just request an account on our Yahoo! China's production server by sending an email to C<agentzh@yahoo.cn>. If you're running your own instance of the OpenResty server, you can use the following command to create an account (named C<foo>) for yourself:
+
+    $ bin/openresty adduser foo
+
+You'll be prompted to enter a password for the C<Admin> role of your C<foo> account.
+
+Throughout this tutorial, we'll assume you own an account named C<foo> whose C<Admin> role's password is C<hello1234>.
+
+=head1 Just Mudding Around
+
+=head1 Importing huge amount of data
+
+=head1 Sharing your data with others
+
+=head1 Keeping a data backup at your localhost
+
+=head1 Where to go from here
+
+=head1 AUTHOR
+
+Agent Zhang (agentzh) C<agentzh@yahoo.cn>
+
+Copyright (c) 2007 by Yahoo! China EEEE Works, Alibaba Inc.
+
