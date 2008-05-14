2644fc4cbe8e23c1bf7a80ef3be5a46c1377dac6
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-14 07:22:00 +0000

    r8981@agentz-office:  agentz | 2008-05-14 15:15:20 +0800  HS RS - added RestyScript.Emitter
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1345 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/RestyScript.hs
+++ haskell/RestyScript.hs
@@ -4,43 +4,7 @@ module RestyScript (
 
 import RestyScript.Parser
 import RestyScript.AST
-import Data.List (intercalate)
-import Text.Printf (printf)
-
-quote :: Char -> String -> String
-quote sep s = [sep] ++ quoteChars s ++ [sep]
-              where quoteChars (x:xs) =
-                        if x == sep || x == '\\'
-                            then x : x : quoteChars xs
-                            else case lookup x escapes of
-                                Just r -> r ++ quoteChars xs
-                                Nothing -> x : quoteChars xs
-                    quoteChars [] = ""
-
-quoteLiteral :: String -> String
-quoteLiteral = quote '\''
-
-quoteIdent :: String -> String
-quoteIdent = quote '"'
-
-emitSql :: SqlVal -> String
-emitSql (String s) = quoteLiteral s
-emitSql (Select cols) = "select " ++ (intercalate ", " $ map emitSql cols)
-emitSql (From models) = "from " ++ (intercalate ", " $ map emitSql models)
-emitSql (Where cond) = "where " ++ (emitSql cond)
-emitSql (Model model) = emitSql model
-emitSql (Column col) = emitSql col
-emitSql (Symbol name) = quoteIdent name
-emitSql (Integer int) = show int
-emitSql (Float float) = printf "%0f" float
-emitSql (OrExpr args) = "(" ++ (intercalate " or " $ map emitSql args) ++ ")"
-emitSql (AndExpr args) = "(" ++ (intercalate " and " $ map emitSql args) ++ ")"
-emitSql (RelExpr (op, lhs, rhs)) = "(" ++ (emitSql lhs) ++ " " ++ op ++ " " ++ (emitSql rhs) ++ ")"
-emitSql (NullClause) = ""
-
-escapes :: [(Char, String)]
-escapes = zipWith ch "\b\n\f\r\t" "bnfrt"
-    where ch a b = (a, '\\':[b])
+import RestyScript.Emitter
 
 compileView :: String -> String -> Either String [String]
 compileView file input = do case readView file input of
