14ebdae02b18bf0b2647357c0754b596faa16a45
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-19 08:07:01 +0000

    r9179@agentz-office:  agentz | 2008-05-19 16:00:53 +0800  added support for command line argument "rs" and "frags"
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1398 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -6,17 +6,30 @@ import qualified RestyScript.Emitter.Stats as St
 
 import System
 import System.IO
+import System.Exit
 
 main :: IO ()
 main = do args <- getArgs
-          input <- hGetContents stdin
-          case compileView "RestyScript" input of
-            Left err -> hPutStrLn stderr err
-            Right vals -> do putStrLn (vals !! 0)
-                             putStrLn (vals !! 1)
+          case args of
+            "rs" : xs -> genRS
+            "frags" : xs -> genFrags
+            otherwise -> die "No command specified"
 
-compileView :: String -> String -> Either String [String]
-compileView file input = do case readView file input of
-                                Left err -> Left $ show err
-                                Right ast -> Right [show ast, RS.emit ast]
+genRS :: IO ()
+genRS = do input <- hGetContents stdin
+           case readView "RestyScript" input of
+             Left err -> die (show err)
+             Right ast -> do putStrLn $ show ast
+                             putStrLn $ RS.emit ast
+
+die :: String -> IO ()
+die msg = do hPutStrLn stderr msg
+             exitWith $ ExitFailure 1
+
+genFrags :: IO ()
+genFrags = do input <- hGetContents stdin
+              case readView "RestyScript" input of
+                  Left err -> hPutStrLn stderr $ show err
+                  Right ast -> do putStrLn $ show ast
+                                  putStrLn $ RS.emit ast
 
