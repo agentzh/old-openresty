06438a50b023fbbe1674db50f11a47f8cca5ffa9
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-19 07:15:16 +0000

    r9175@agentz-office:  agentz | 2008-05-19 15:09:06 +0800  HS RS - added the traverse function and Visit typeclass to AST.hs
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1396 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/RestyScript/AST.hs
+++ haskell/src/RestyScript/AST.hs
@@ -1,5 +1,7 @@
 module RestyScript.AST (
-    SqlVal(..)
+    SqlVal(..),
+    traverse,
+    Visit
 ) where
 
 data SqlVal = SetOp String SqlVal SqlVal
@@ -14,7 +16,6 @@ data SqlVal = SetOp String SqlVal SqlVal
             | OrderPair SqlVal String
             | GroupBy SqlVal
             | Alias SqlVal SqlVal
-            | Term SqlVal
             | Column SqlVal
             | Model SqlVal
             | Symbol String
@@ -31,4 +32,34 @@ data SqlVal = SetOp String SqlVal SqlVal
             | Null
             | AnyColumn
                 deriving (Ord, Eq, Show)
+
+class Visit a where
+
+traverse :: (Visit a) => (SqlVal->a) -> (a->a->a) -> SqlVal -> a
+traverse visit merge node =
+    let mergeAll = foldl1 merge
+        cur = visit node
+        self = traverse visit merge
+    in case node of
+        SetOp _ lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        Query q -> mergeAll $ cur : map self q
+        Select s -> mergeAll $ cur : map self s
+        From f -> mergeAll $ cur : map self f
+        Where w -> merge cur $ self w
+        Limit l -> merge cur $ self l
+        Offset o -> merge cur $ self o
+        TypeCast e t -> mergeAll [cur, self e, self t]
+        OrderBy o -> mergeAll $ cur : map self o
+        OrderPair col _ -> merge cur (self col)
+        GroupBy g -> merge cur (self g)
+        Alias col alias -> mergeAll [cur, self col, self alias]
+        Column c -> merge cur (self c)
+        Model m -> merge cur (self m)
+        QualifiedColumn lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        FuncCall _ args -> mergeAll $ cur : map self args
+        Compare _ lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        Arith _ lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        Or lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        And lhs rhs -> mergeAll [cur, self lhs, self rhs]
+        otherwise -> cur
 
