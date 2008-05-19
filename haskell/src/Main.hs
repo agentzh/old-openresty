127b7250f7376325ce37e40a2393dfc442c16f2e
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-19 07:53:57 +0000

    r9177@agentz-office:  agentz | 2008-05-19 15:47:48 +0800  HS RS - merged Main.hs and RestyScript.hs
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1397 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,13 +1,22 @@
 module Main where
 
+import RestyScript.Parser
+import qualified RestyScript.Emitter.RestyScript as RS
+import qualified RestyScript.Emitter.Stats as St
+
 import System
-import RestyScript
 import System.IO
 
 main :: IO ()
-main = do input <- hGetContents stdin
+main = do args <- getArgs
+          input <- hGetContents stdin
           case compileView "RestyScript" input of
             Left err -> hPutStrLn stderr err
             Right vals -> do putStrLn (vals !! 0)
                              putStrLn (vals !! 1)
+
+compileView :: String -> String -> Either String [String]
+compileView file input = do case readView file input of
+                                Left err -> Left $ show err
+                                Right ast -> Right [show ast, RS.emit ast]
 
