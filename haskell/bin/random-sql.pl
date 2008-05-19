50703806171e3131bb57545a069fd8eef6897386
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-19 02:59:27 +0000

    r9087@agentz-office:  agentz | 2008-05-19 10:53:17 +0800  added bin/random-sql.pl to generate random sql instances for testing the minisql compiler
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1391 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ haskell/bin/random-sql.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use Parse::RandGen::Regexp;
+
+my $list_len = 10;
+my $cmp_a = 1;
+my $cmp_b = 2;
+my $ident = qr/"[A-Za-z]\w*"/;
+my $ident_list = qr/$ident(\s*,\s*$ident){$list_len,}/;
+my $int = qr/[-+]?\d+/;
+my $float = qr/[-+]?(\d+\.(\d+)?|\.\d+)/;
+my $number = qr/$int|$float/;
+my $atom = qr/$number|$ident/;
+my $rel_op = qr/>|>=|<|<=|!=|<>| like /;
+my $cmp = qr/$atom\s*$rel_op\s*$atom/;
+my $logic_op = qr/and|or/;
+my $logic_exp = qr/$cmp\s+$logic_op\s+$cmp/;
+my $cond = qr/$logic_exp(\s+$logic_op\s+$logic_exp){$cmp_a,$cmp_b}/;
+my $gen = Parse::RandGen::Regexp->new(qr/select $ident_list from $ident_list where $cond/);
+print $gen->pick;
+
