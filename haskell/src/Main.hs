03d6e8e3496a4c974d83d40f872ceb1776a927fa
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-20 02:40:17 +0000

    r9191@agentz-office:  agentz | 2008-05-20 10:34:09 +0800  HS RS - now the command line accepts multiple commands
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1404 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import RestyScript.Parser
+import RestyScript.AST
 import qualified RestyScript.Emitter.RestyScript as RS
 import qualified RestyScript.Emitter.Stats as St
 
@@ -8,28 +9,29 @@ import System
 import System.IO
 import System.Exit
 
+argHandles :: [(String, SqlVal -> IO ())]
+argHandles = [
+    ("rs", putStrLn . RS.emit),
+    ("stats", putStrLn . show . St.emit),
+    ("ast", putStrLn . show)]
+
 main :: IO ()
 main = do args <- getArgs
-          case args of
-            "rs" : xs -> genRS
-            "stats" : xs -> genFrags
-            x : xs -> die $ "Unknown command: " ++ x
-            otherwise -> die "No command specified"
+          if null args
+            then die "No command specified."
+            else do input <- hGetContents stdin
+                    case readView "RestyScript" input of
+                        Left err -> die (show err)
+                        Right ast -> processArgs args ast
 
-genRS :: IO ()
-genRS = do input <- hGetContents stdin
-           case readView "RestyScript" input of
-             Left err -> die (show err)
-             Right ast -> do putStrLn $ show ast
-                             putStrLn $ RS.emit ast
+processArgs :: [String] -> SqlVal -> IO ()
+processArgs [] _ = return ()
+processArgs (a:as) ast =
+    case lookup a argHandles of
+        Just hdl -> hdl ast >> processArgs as ast
+        Nothing -> die $ "Unknown command: " ++ a
 
 die :: String -> IO ()
 die msg = do hPutStrLn stderr msg
              exitWith $ ExitFailure 1
-
-genFrags :: IO ()
-genFrags = do input <- hGetContents stdin
-              case readView "RestyScript" input of
-                  Left err -> hPutStrLn stderr $ show err
-                  Right ast -> putStrLn $ show $ St.emit ast
 
