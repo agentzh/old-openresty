052d480e9eba16558151651c8cb1e72ac34db514
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-22 05:35:07 +0000

    r9295@agentz-office:  agentz | 2008-05-22 13:28:56 +0800  HS RS - replaced String with Data.ByteString in the RS emitter, resulting in 70+Makefile performance boost
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1429 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/RestyScript/Emitter/RestyScript.hs
+++ haskell/src/RestyScript/Emitter/RestyScript.hs
@@ -5,40 +5,43 @@ import RestyScript.Util
 
 import Data.List (intercalate)
 import Text.Printf (printf)
+import qualified Data.ByteString.Char8 as B
 
-emitForList ls = intercalate ", " $ map emit ls
+emitForList :: [SqlVal] -> B.ByteString
+emitForList ls = B.intercalate (B.pack ", ") $ map emit ls
 
-emit :: SqlVal -> String
+emit :: SqlVal -> B.ByteString
 emit node = case node of
-    TypeCast e t -> (emit e) ++ "::" ++ (emit t)
-    SetOp op lhs rhs -> "((" ++ (emit lhs) ++ ") " ++ op ++
-                        " (" ++ (emit rhs) ++ "))"
-    Query lst -> unwords $ map emit lst
-    String s -> quoteLiteral s
-    Variable _ v -> '$' : v
-    FuncCall f args -> (emit f) ++
-                                  "(" ++ (emitForList args) ++ ")"
-    QualifiedColumn model col -> (emit model) ++ "." ++ (emit col)
+    TypeCast e t -> B.concat [emit e, bs "::", emit t]
+    SetOp op lhs rhs -> B.concat [bs "((", emit lhs, bs ") ", bs op,
+                        bs " (", emit rhs, bs "))"]
+    Query lst -> B.unwords $ map emit lst
+    String s -> bs $ quoteLiteral s
+    Variable _ v -> '$' `B.cons` (bs v)
+    FuncCall f args -> B.concat [emit f,
+                                  bs "(", emitForList args, bs ")"]
+    QualifiedColumn model col -> B.concat [emit model, bs ".", emit col]
 
-    Select cols -> "select " ++ (emitForList cols)
-    From models -> "from " ++ (emitForList models)
-    Where cond -> "where " ++ (emit cond)
-    OrderBy pairs -> "order by " ++ (emitForList pairs)
-    GroupBy col -> "group by " ++ emit col
-    Limit lim -> "limit " ++ emit lim
-    Offset offset -> "offset " ++ emit offset
+    Select cols -> B.concat [bs "select ", emitForList cols]
+    From models -> B.concat [bs "from ", emitForList models]
+    Where cond -> B.concat [bs "where ", emit cond]
+    OrderBy pairs -> B.concat [bs "order by ", emitForList pairs]
+    GroupBy col -> B.concat [bs "group by ", emit col]
+    Limit lim -> B.concat [bs "limit ", emit lim]
+    Offset offset -> B.concat [bs "offset ", emit offset]
 
-    OrderPair col dir -> (emit col) ++ " " ++ dir
+    OrderPair col dir -> B.concat [emit col, bs " ", bs dir]
     Model model -> emit model
     Column col -> emit col
-    Symbol name -> quoteIdent name
-    Integer int -> show int
-    Float float -> printf "%0f" float
-    Or a b -> "(" ++ (emit a) ++ " or " ++  (emit b) ++ ")"
-    And a b -> "(" ++ (emit a) ++ " and " ++ (emit b) ++ ")"
-    Compare op lhs rhs -> (emit lhs) ++ " " ++ op ++ " " ++ (emit rhs)
-    Arith op lhs rhs -> "(" ++ (emit lhs) ++ " " ++ op ++ " " ++ (emit rhs) ++ ")"
-    Alias col alias -> (emit col) ++ " as " ++ (emit alias)
-    Null -> ""
-    AnyColumn -> "*"
+    Symbol name -> bs $ quoteIdent name
+    Integer int -> bs $ show int
+    Float float -> bs $ printf "%0f" float
+    Or a b -> B.concat [bs "(", emit a, bs " or ", emit b, bs ")"]
+    And a b -> B.concat [bs "(", emit a, bs " and ", emit b, bs ")"]
+    Compare op lhs rhs -> B.concat [emit lhs, bs " ", bs op, bs " ", emit rhs]
+    Arith op lhs rhs -> B.concat [bs "(", emit lhs, bs " ", bs op, bs " ", emit rhs, bs ")"]
+    Alias col alias -> B.concat [emit col, bs " as ", emit alias]
+    Null -> B.empty
+    AnyColumn -> bs "*"
+    where bs = B.pack
 
