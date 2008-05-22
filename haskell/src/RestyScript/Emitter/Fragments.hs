d9976cacdc63a251449676e7478a63ff3e112244
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-22 08:20:05 +0000

    r9301@agentz-office:  agentz | 2008-05-22 16:12:27 +0800  HS RS - updated README to state that we need at least bytestring 0.9.1.0
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1432 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/RestyScript/Emitter/Fragments.hs
+++ haskell/src/RestyScript/Emitter/Fragments.hs
@@ -17,16 +17,16 @@ data VarType = VTLiteral | VTSymbol | VTUnknown
     deriving (Ord, Eq, Show)
 
 instance JSON VarType where
-    showJSON VTSymbol = showJSON "symbol"
-    showJSON VTLiteral = showJSON "literal"
-    showJSON VTUnknown = showJSON "unknown"
+    showJSON VTSymbol = showJSON $ B.unpack "symbol"
+    showJSON VTLiteral = showJSON $ B.unpack "literal"
+    showJSON VTUnknown = showJSON $ B.unpack "unknown"
     readJSON = undefined
 
 data Fragment = FVariable String VarType | FString B.ByteString
     deriving (Ord, Eq, Show)
 
 instance JSON Fragment where
-    showJSON (FString s) = showJSON s
+    showJSON (FString s) = showJSON $ B.unpack s
     showJSON (FVariable v t) = JSArray [showJSON v, showJSON t]
     readJSON = undefined
 
@@ -36,16 +36,16 @@ bs = B.pack
 emit :: SqlVal -> [Fragment]
 emit node =
     case node of
-        TypeCast (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTUnknown, FString $ bs "::", FVariable v2 VTSymbol]
-        TypeCast (Variable _ v1) t -> merge [FVariable v1 VTUnknown, FString $ bs "::"] $ emit t
-        TypeCast e (Variable _ v2) -> merge (emit e) [FString $ bs "::", FVariable v2 VTSymbol]
+        TypeCast (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTUnknown, FString $ "::", FVariable v2 VTSymbol]
+        TypeCast (Variable _ v1) t -> merge [FVariable v1 VTUnknown, FString $ "::"] $ emit t
+        TypeCast e (Variable _ v2) -> merge (emit e) [FString $ "::", FVariable v2 VTSymbol]
         TypeCast e t -> join "::" $ [emit e, emit t]
 
         SetOp op lhs rhs -> mergeAll [
-            str "((", emit lhs, str $ ") " ++ op ++ " (",
+            str "((", emit lhs, str $ B.concat [") ", bs op, " ("],
             emit rhs, str "))"]
         Query lst -> join " " $ map emit lst
-        String s -> str $ quoteLiteral s
+        String s -> str $ bs $ quoteLiteral s
 
         Model (Variable _ v) -> [FVariable v VTSymbol]
         Model m -> emit m
@@ -54,41 +54,41 @@ emit node =
         Column col -> emit col
 
         Variable _ v -> [FVariable v VTUnknown]
-        Alias e (Variable _ v) -> merge (emit e) [FString $ bs " as ", FVariable v VTSymbol]
+        Alias e (Variable _ v) -> merge (emit e) [FString $ " as ", FVariable v VTSymbol]
 
-        QualifiedColumn (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTSymbol, FString $ bs ".", FVariable v2 VTSymbol]
-        QualifiedColumn (Variable _ v) c -> merge [FVariable v VTSymbol, FString $ bs "."] (emit c)
-        QualifiedColumn m (Variable _ v) -> merge (emit m) [FString $ bs ".", FVariable v VTSymbol]
+        QualifiedColumn (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTSymbol, FString $ ".", FVariable v2 VTSymbol]
+        QualifiedColumn (Variable _ v) c -> merge [FVariable v VTSymbol, FString $ "."] (emit c)
+        QualifiedColumn m (Variable _ v) -> merge (emit m) [FString $ ".", FVariable v VTSymbol]
 
-        FuncCall (Variable _ v) args -> mergeAll $ [[FVariable v VTSymbol, FString $ bs "("], emitForList args, str ")"]
+        FuncCall (Variable _ v) args -> mergeAll $ [[FVariable v VTSymbol, FString $ "("], emitForList args, str ")"]
         FuncCall f args -> mergeAll $ [emit f, str "(", emitForList args, str ")"]
         QualifiedColumn model col -> mergeAll [emit model, str ".", emit col]
         Select cols -> mergeAll $ [str "select ", emitForList cols]
         From models -> mergeAll $ [str "from ", emitForList models]
         Where cond -> mergeAll $ [str "where ", emit cond]
         OrderBy pairs -> mergeAll $ [str "order by ", emitForList pairs]
-        Symbol name -> str $ quoteIdent name
+        Symbol name -> str $ bs $ quoteIdent name
 
-        GroupBy (Variable _ v) -> [FString $ bs "group by ", FVariable v VTSymbol]
+        GroupBy (Variable _ v) -> [FString $ "group by ", FVariable v VTSymbol]
         GroupBy col -> merge (str "group by ") (emit col)
 
-        Limit (Variable _ v) -> [FString $ bs "limit ", FVariable v VTLiteral]
+        Limit (Variable _ v) -> [FString $ "limit ", FVariable v VTLiteral]
         Limit lim -> merge (str "limit ") (emit lim)
 
-        Offset (Variable _ v) -> [FString $ bs "offset ", FVariable v VTLiteral]
+        Offset (Variable _ v) -> [FString $ "offset ", FVariable v VTLiteral]
         Offset offset -> merge (str "offset ") (emit offset)
 
         Alias col alias -> mergeAll [emit col, str " as ", emit alias]
         AnyColumn -> str "*"
-        OrderPair col dir -> mergeAll [emit col, str (" " ++ dir)]
-        Integer int -> str $ show int
-        Float float -> str $ printf "%0f" float
+        OrderPair col dir -> mergeAll [emit col, str $ B.concat [" ", bs dir]]
+        Integer int -> str $ bs $ show int
+        Float float -> str $ bs $ printf "%0f" float
         Or a b -> mergeAll [str "(", emit a, str " or ", emit b, str ")"]
         And a b -> mergeAll [str "(", emit a, str " and ", emit b, str ")"]
-        Compare op lhs rhs -> mergeAll [emit lhs, str $ " " ++ op ++ " ", emit rhs]
-        Arith op lhs rhs -> mergeAll [str "(", emit lhs, str $ " " ++ op ++ " ", emit rhs, str ")"]
+        Compare op lhs rhs -> mergeAll [emit lhs, str $ B.concat [" ", bs op, " "], emit rhs]
+        Arith op lhs rhs -> mergeAll [str "(", emit lhs, str $ B.concat [" ", bs op, " "], emit rhs, str ")"]
         Null -> str ""
-    where str s = [FString $ bs s]
+    where str s = [FString s]
           emitForList ls = join ", " $ map emit ls
 
 mergeAll :: [[Fragment]] -> [Fragment]
--- haskell/src/RestyScript/Emitter/RestyScript.hs
+++ haskell/src/RestyScript/Emitter/RestyScript.hs
@@ -11,36 +11,36 @@ emitForList ls = B.intercalate (B.pack ", ") $ map emit ls
 
 emit :: SqlVal -> B.ByteString
 emit node = case node of
-    TypeCast e t -> B.concat [emit e, bs "::", emit t]
-    SetOp op lhs rhs -> B.concat [bs "((", emit lhs, bs ") ", bs op,
-                        bs " (", emit rhs, bs "))"]
+    TypeCast e t -> B.concat [emit e, "::", emit t]
+    SetOp op lhs rhs -> B.concat ["((", emit lhs, ") ", bs op,
+                        " (", emit rhs, "))"]
     Query lst -> B.unwords $ map emit lst
     String s -> bs $ quoteLiteral s
     Variable _ v -> '$' `B.cons` (bs v)
     FuncCall f args -> B.concat [emit f,
-                                  bs "(", emitForList args, bs ")"]
-    QualifiedColumn model col -> B.concat [emit model, bs ".", emit col]
+                                  "(", emitForList args, ")"]
+    QualifiedColumn model col -> B.concat [emit model, ".", emit col]
 
-    Select cols -> B.concat [bs "select ", emitForList cols]
-    From models -> B.concat [bs "from ", emitForList models]
-    Where cond -> B.concat [bs "where ", emit cond]
-    OrderBy pairs -> B.concat [bs "order by ", emitForList pairs]
-    GroupBy col -> B.concat [bs "group by ", emit col]
-    Limit lim -> B.concat [bs "limit ", emit lim]
-    Offset offset -> B.concat [bs "offset ", emit offset]
+    Select cols -> B.concat ["select ", emitForList cols]
+    From models -> B.concat ["from ", emitForList models]
+    Where cond -> B.concat ["where ", emit cond]
+    OrderBy pairs -> B.concat ["order by ", emitForList pairs]
+    GroupBy col -> B.concat ["group by ", emit col]
+    Limit lim -> B.concat ["limit ", emit lim]
+    Offset offset -> B.concat ["offset ", emit offset]
 
-    OrderPair col dir -> B.concat [emit col, bs " ", bs dir]
+    OrderPair col dir -> B.concat [emit col, " ", bs dir]
     Model model -> emit model
     Column col -> emit col
     Symbol name -> bs $ quoteIdent name
     Integer int -> bs $ show int
     Float float -> bs $ printf "%0f" float
-    Or a b -> B.concat [bs "(", emit a, bs " or ", emit b, bs ")"]
-    And a b -> B.concat [bs "(", emit a, bs " and ", emit b, bs ")"]
-    Compare op lhs rhs -> B.concat [emit lhs, bs " ", bs op, bs " ", emit rhs]
-    Arith op lhs rhs -> B.concat [bs "(", emit lhs, bs " ", bs op, bs " ", emit rhs, bs ")"]
-    Alias col alias -> B.concat [emit col, bs " as ", emit alias]
+    Or a b -> B.concat ["(", emit a, " or ", emit b, ")"]
+    And a b -> B.concat ["(", emit a, " and ", emit b, ")"]
+    Compare op lhs rhs -> B.concat [emit lhs, " ", bs op, " ", emit rhs]
+    Arith op lhs rhs -> B.concat ["(", emit lhs, " ", bs op, " ", emit rhs, ")"]
+    Alias col alias -> B.concat [emit col, " as ", emit alias]
     Null -> B.empty
-    AnyColumn -> bs "*"
+    AnyColumn -> "*"
     where bs = B.pack
 
