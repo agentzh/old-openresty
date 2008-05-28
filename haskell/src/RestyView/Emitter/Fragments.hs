322a6dcf8eac32609518e97d03c63ae11f6e3e74
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-05-28 10:57:41 +0000

    r9443@agentz-office:  agentz | 2008-05-28 18:51:14 +0800  restyview - introduced custom operators <+> and ~~ to simplify the emitter code
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1462 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/RestyView/Emitter/Fragments.hs
+++ haskell/src/RestyView/Emitter/Fragments.hs
@@ -33,17 +33,20 @@ instance JSON Fragment where
 bs :: String -> B.ByteString
 bs = B.pack
 
+(~~) = B.append
+(<+>) = merge
+
 emit :: SqlVal -> [Fragment]
 emit node =
     case node of
         TypeCast (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTUnknown, FString $ "::", FVariable v2 VTSymbol]
-        TypeCast (Variable _ v1) t -> merge [FVariable v1 VTUnknown, FString $ "::"] $ emit t
+        TypeCast (Variable _ v1) t -> [FVariable v1 VTUnknown, FString $ "::"] <+> emit t
         TypeCast e (Variable _ v2) -> merge (emit e) [FString $ "::", FVariable v2 VTSymbol]
         TypeCast e t -> join "::" $ [emit e, emit t]
 
-        SetOp op lhs rhs -> mergeAll [
-            str "((", emit lhs, str $ B.concat [") ", bs op, " ("],
-            emit rhs, str "))"]
+        SetOp op lhs rhs ->
+            str "((" <+> emit lhs <+> (str $ ") " ~~ bs op ~~ " (") <+>
+            emit rhs <+> str "))"
         Query lst -> join " " $ map emit lst
         String s -> str $ bs $ quoteLiteral s
 
@@ -54,49 +57,45 @@ emit node =
         Column col -> emit col
 
         Variable _ v -> [FVariable v VTUnknown]
-        Alias e (Variable _ v) -> merge (emit e) [FString $ " as ", FVariable v VTSymbol]
+        Alias e (Variable _ v) -> emit e <+> [FString $ " as ", FVariable v VTSymbol]
 
         QualifiedColumn (Variable _ v1) (Variable _ v2) -> [FVariable v1 VTSymbol, FString $ ".", FVariable v2 VTSymbol]
-        QualifiedColumn (Variable _ v) c -> merge [FVariable v VTSymbol, FString $ "."] (emit c)
-        QualifiedColumn m (Variable _ v) -> merge (emit m) [FString $ ".", FVariable v VTSymbol]
+        QualifiedColumn (Variable _ v) c -> [FVariable v VTSymbol, FString $ "."] <+> emit c
+        QualifiedColumn m (Variable _ v) -> emit m <+> [FString $ ".", FVariable v VTSymbol]
 
-        FuncCall (Variable _ v) args -> mergeAll $ [[FVariable v VTSymbol, FString $ "("], emitForList args, str ")"]
-        FuncCall f args -> mergeAll $ [emit f, str "(", emitForList args, str ")"]
-        QualifiedColumn model col -> mergeAll [emit model, str ".", emit col]
-        Select cols -> mergeAll $ [str "select ", emitForList cols]
-        From models -> mergeAll $ [str "from ", emitForList models]
-        Where cond -> mergeAll $ [str "where ", emit cond]
-        OrderBy pairs -> mergeAll $ [str "order by ", emitForList pairs]
+        FuncCall (Variable _ v) args -> [FVariable v VTSymbol, FString $ "("] <+> emitForList args <+> str ")"
+        FuncCall f args -> emit f <+> str "(" <+> emitForList args <+> str ")"
+        QualifiedColumn model col -> emit model <+> str "." <+> emit col
+        Select cols -> str "select " <+> emitForList cols
+        From models -> str "from " <+> emitForList models
+        Where cond -> str "where " <+> emit cond
+        OrderBy pairs -> str "order by " <+> emitForList pairs
         Symbol name -> str $ bs $ quoteIdent name
 
         GroupBy (Variable _ v) -> [FString $ "group by ", FVariable v VTSymbol]
-        GroupBy col -> merge (str "group by ") (emit col)
+        GroupBy col -> str "group by " <+> emit col
 
         Limit (Variable _ v) -> [FString $ "limit ", FVariable v VTLiteral]
-        Limit lim -> merge (str "limit ") (emit lim)
+        Limit lim -> str "limit " <+> emit lim
 
         Offset (Variable _ v) -> [FString $ "offset ", FVariable v VTLiteral]
-        Offset offset -> merge (str "offset ") (emit offset)
+        Offset offset -> str "offset " <+> emit offset
 
-        Alias col alias -> mergeAll [emit col, str " as ", emit alias]
+        Alias col alias -> emit col <+> str " as " <+> emit alias
         AnyColumn -> str "*"
-        OrderPair col dir -> mergeAll [emit col, str $ B.concat [" ", bs dir]]
+        OrderPair col dir -> emit col <+> str (" " ~~ bs dir)
         Integer int -> str $ bs $ show int
         Float float -> str $ bs $ printf "%0f" float
-        Or a b -> mergeAll [str "(", emit a, str " or ", emit b, str ")"]
-        And a b -> mergeAll [str "(", emit a, str " and ", emit b, str ")"]
-        Compare op lhs rhs -> mergeAll [emit lhs, str $ B.concat [" ", bs op, " "], emit rhs]
-        Arith op lhs rhs -> mergeAll [str "(", emit lhs, str $ B.concat [" ", bs op, " "], emit rhs, str ")"]
-        Minus val -> mergeAll [str "(-", emit val, str ")"]
+        Or a b -> str "(" <+> emit a <+> str " or " <+> emit b <+> str ")"
+        And a b -> str "(" <+> emit a <+> str " and " <+> emit b <+> str ")"
+        Compare op lhs rhs -> emit lhs <+> (str $ " " ~~ bs op ~~ " ") <+> emit rhs
+        Arith op lhs rhs -> str "(" <+> emit lhs <+> (str $ " " ~~ bs op ~~ " ") <+> emit rhs <+> str ")"
+        Minus val -> str "(-" <+> emit val <+> str ")"
         Plus val -> emit val
-        Not val -> mergeAll [str "(not ", emit val, str ")"]
+        Not val -> str "(not " <+> emit val <+> str ")"
         Null -> str ""
     where str s = [FString s]
           emitForList ls = join ", " $ map emit ls
-
-mergeAll :: [[Fragment]] -> [Fragment]
-mergeAll [] = []
-mergeAll ls = foldl1 merge ls
 
 merge :: [Fragment] -> [Fragment] -> [Fragment]
 merge [] b = b
@@ -107,7 +106,7 @@ merge a b = case (a !! (length a - 1), (head b)) of
 
 join :: String -> [[Fragment]] -> [Fragment]
 join sep [] = []
-join sep lst = mergeAll $ intersperse [FString $ bs sep] lst
+join sep lst = foldl1 merge $ intersperse [FString $ bs sep] lst
 
 emitJSON :: SqlVal -> String
 emitJSON = encode . emit
