684875b389b3fa90301f3e4a6cdfdb62a7af9407
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-06-04 03:18:03 +0000

    r9527@agentz-office:  agentz | 2008-06-04 11:14:28 +0800  restyaction - committed missing .hs files
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1505 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ haskell/src/RestyScript/Parser/Action.hs
@@ -0,0 +1,114 @@
+module RestyScript.Parser.Action (
+    readAction
+) where
+
+import RestyScript.Parser
+import RestyScript.AST.Action
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Monad (liftM, msum)
+
+readAction :: String -> String -> Either ParseError RSVal
+readAction = parse parseAction
+
+parseAction :: Parser RSVal
+parseAction = do spaces
+                 stmts <- sepEndBy1 parseStmt $ many1 (char ';' >> spaces)
+                 eof
+                 return $ Action stmts
+
+parseStmt :: Parser RSVal
+parseStmt = parseDelete
+        <|> parseUpdate
+        <|> parseHttp
+        <?> "RestyAction statement"
+
+httpCmds = ["GET", "PUT", "DELETE", "POST"]
+
+parseHttp :: Parser RSVal
+parseHttp = do cmd <- msum $ map keyword httpCmds
+               spaces
+               url <- parseLitExpr
+               content <- option Null parseJSON
+               return $ HttpCmd cmd url content
+
+litOpTable = [
+            [ arithOp "||" ]
+            ]
+    where
+        op s f assoc
+           = Infix (do { reservedOp s; spaces; return f} <?> "operator") assoc
+        arithOp s
+           = op s (Arith s) AssocLeft
+
+
+parseLitExpr :: Parser RSVal
+parseLitExpr = buildExpressionParser litOpTable parseLitAtom
+        <?> "literal expression"
+
+parseLitAtom :: Parser RSVal
+parseLitAtom = parseNumber
+           <|> parseString
+           <|> parseDString
+           <|> parseVariable
+           <|> parens parseLitExpr
+
+-- double quoted strings in the context of HTTP commands
+parseDString :: Parser RSVal
+parseDString = do s <- between (char '"' >> spaces) (char '"' >> spaces)
+                        $ many $ quotedChar '"'
+                  spaces
+                  return $ String s
+
+parseJSON :: Parser RSVal
+parseJSON = parseObject
+        <|> parseArray
+        <|> parseLitExpr
+        <?> "JSON content"
+
+parseObject :: Parser RSVal
+parseObject = do obj <- between (char '{' >> spaces) (char '}' >> spaces)
+                    $ sepBy1 parsePair (char ',' >> spaces)
+                 spaces
+                 return $ Object obj
+
+parsePair :: Parser RSVal
+parsePair = do key <- parseKey
+               char ':'
+               spaces
+               value <- parseJSON
+               return $ Pair key value
+
+parseKey :: Parser RSVal
+parseKey = parseLitExpr
+       <|> liftM String symbol
+       <?> "JSON hash key"
+
+parseArray :: Parser RSVal
+parseArray = do lst <- between (char '[' >> spaces) (char ']' >> spaces)
+                    $ sepBy parseJSON (char ',' >> spaces)
+                spaces
+                return $ Array lst
+
+parseDelete :: Parser RSVal
+parseDelete = do keyword "delete" >> spaces >> keyword "from" >> spaces
+                 model <- parseModel
+                 cond <- parseWhere
+                 return $ Delete model cond
+          <?> "SQL delete statement"
+
+parseUpdate :: Parser RSVal
+parseUpdate = do keyword "update" >> spaces
+                 model <- parseModel
+                 assign <- keyword "set" >> spaces >> parseAssign
+                 cond <- option Null parseWhere
+                 return $ Update model assign cond
+          <?> "SQL update statement"
+
+parseAssign :: Parser RSVal
+parseAssign = do col <- parseColumn
+                 char '=' >> spaces
+                 expr <- parseExpr
+                 return $ Assign col expr
+          <?> "Column assignment"
+
