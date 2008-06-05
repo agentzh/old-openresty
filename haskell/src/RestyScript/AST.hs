e62416a667224a4b564b78a26fd3f1df09315b82
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-06-05 09:50:20 +0000

    r9578@agentz-office:  agentz | 2008-06-05 17:46:45 +0800  restyscript - minor optimization by making datatypes more strict
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1531 b2c27d59-8999-dc11-853b-000bcdefe56b

--- haskell/src/RestyScript/AST.hs
+++ haskell/src/RestyScript/AST.hs
@@ -5,48 +5,48 @@ module RestyScript.AST (
 import Text.ParserCombinators.Parsec.Pos (SourcePos)
 
 -- RestyScript AST datatype
-data RSVal = SetOp !String RSVal RSVal
-           | Query [RSVal]
-           | Select [RSVal] -- Select entries
-           | From [RSVal]
-           | Where RSVal
-           | Limit RSVal
-           | Offset RSVal
-           | TypeCast RSVal RSVal
-           | OrderBy [RSVal]
-           | OrderPair RSVal !String
-           | GroupBy RSVal
-           | Alias RSVal RSVal
-           | Column RSVal
-           | Model RSVal
+data RSVal = SetOp !String !RSVal !RSVal
+           | Query ![RSVal]
+           | Select ![RSVal] -- Select entries
+           | From ![RSVal]
+           | Where !RSVal
+           | Limit !RSVal
+           | Offset !RSVal
+           | TypeCast !RSVal !RSVal
+           | OrderBy ![RSVal]
+           | OrderPair !RSVal !String
+           | GroupBy !RSVal
+           | Alias !RSVal !RSVal
+           | Column !RSVal
+           | Model !RSVal
            | Symbol !String
-           | QualifiedColumn RSVal RSVal
+           | QualifiedColumn !RSVal !RSVal
            | Integer !Int
            | Float !Double
            | String !String
-           | Variable SourcePos !String
-           | FuncCall RSVal [RSVal]
-           | Compare !String RSVal RSVal
-           | Arith !String RSVal RSVal
-           | Minus RSVal
-           | Plus RSVal
-           | Not RSVal
-           | Or RSVal RSVal
-           | And RSVal RSVal
+           | Variable !SourcePos !String
+           | FuncCall !RSVal ![RSVal]
+           | Compare !String !RSVal !RSVal
+           | Arith !String !RSVal !RSVal
+           | Minus !RSVal
+           | Plus !RSVal
+           | Not !RSVal
+           | Or !RSVal !RSVal
+           | And !RSVal !RSVal
            | Null
            | AnyColumn
-           | Action [RSVal]
-           | Delete RSVal RSVal -- Delete model cond
-           | Update RSVal RSVal RSVal -- Update model assign cond
-           | Assign RSVal RSVal -- Assign col expr
-           | HttpCmd !String RSVal RSVal -- HttpCmd method url content
-           | Object [RSVal]  -- Object [pair]
-           | Pair RSVal RSVal -- Pair key value
-           | Array [RSVal]  -- Array [elem]
-           | Concat RSVal RSVal
-           | Distinct [RSVal]
-           | All [RSVal]
-               deriving (Ord, Eq, Show)
+           | Action ![RSVal]
+           | Delete !RSVal !RSVal -- Delete model cond
+           | Update !RSVal !RSVal !RSVal -- Update model assign cond
+           | Assign !RSVal !RSVal -- Assign col expr
+           | HttpCmd !String !RSVal !RSVal -- HttpCmd method url content
+           | Object ![RSVal]  -- Object [pair]
+           | Pair !RSVal !RSVal -- Pair key value
+           | Array ![RSVal]  -- Array [elem]
+           | Concat !RSVal !RSVal
+           | Distinct ![RSVal]
+           | All ![RSVal]
+               deriving (Eq, Show)
 
 traverse :: (RSVal->a) -> (a->a->a) -> RSVal -> a
 traverse visit merge node =
