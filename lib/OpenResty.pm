7b3bcf6d89b566fab65fdee14bc3b872c1e47650
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-06-17 06:22:00 +0000

    r9671@agentz-office:  agentz | 2008-06-17 14:17:56 +0800  updated pgmocked-data
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1578 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty.pm
+++ lib/OpenResty.pm
@@ -1,6 +1,6 @@
 package OpenResty;
 
-our $VERSION = '0.003002';
+our $VERSION = '0.003003';
 
 use strict;
 use warnings;
@@ -552,7 +552,7 @@ OpenResty - General-purpose web service platform for web applications
 
 =head1 VERSION
 
-This document describes OpenResty 0.3.2 released on June 3, 2008.
+This document describes OpenResty 0.3.3 released on June 17, 2008.
 
 =head1 DESCRIPTION
 
