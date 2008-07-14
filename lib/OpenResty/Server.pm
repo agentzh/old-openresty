9a1d4a16cc73127bd315ce4265ac1f61b81b2cf7
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-07-14 07:16:52 +0000

    r10374@agentz-office:  agentz | 2008-07-14 15:12:20 +0800  added some POD to OpenResty::Server
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1716 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty/Server.pm
+++ lib/OpenResty/Server.pm
@@ -10,4 +10,24 @@ sub handle_request {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+OpenResty::Server - Standalone server based on HTTP::Server::Simple for OpenResty
+
+=head1 INHERITANCE
+
+    OpenResty::Server
+        ISA HTTP::Server::Simple::CGI
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Agent Zhang (agentzh) C<< <agentzh@yahoo.cn> >>.
+
+=head1 SEE ALSO
+
+L<openresty>, L<HTTP::Server::Simple::CGI>, L<OpenResty>.
 
