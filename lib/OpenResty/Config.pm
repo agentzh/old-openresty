efa5f744a55a3579853c0965beffcef74438764b
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-07-15 08:25:43 +0000

    r10398@agentz-office:  agentz | 2008-07-15 16:21:05 +0800  added some POD to OpenResty::Config
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1726 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty/Config.pm
+++ lib/OpenResty/Config.pm
@@ -56,15 +56,95 @@ sub init {
         warn "backend.type=mmap\n" if $do_echo;
         $OpenResty::Config{'cache.type'} = 'mmap';
     }
-    $OpenResty::Limits::RECORD_LIMIT = to_num $OpenResty::Config{'frontend.row_limit'};
-    $OpenResty::Limits::INSERT_LIMIT = to_num $OpenResty::Config{'frontend.bulk_insert_limit'};
-    $OpenResty::Limits::COLUMN_LIMIT = to_num $OpenResty::Config{'frontend.column_limit'};
-    $OpenResty::Limits::MODEL_LIMIT = to_num $OpenResty::Config{'frontend.model_limit'};
-    $OpenResty::Limits::POST_LEN_LIMIT = to_num $OpenResty::Config{'frontend.post_len_limit'};
+    $OpenResty::Limits::RECORD_LIMIT = to_num
+    $OpenResty::Config{'frontend.row_limit'};
+    $OpenResty::Limits::INSERT_LIMIT = to_num
+    $OpenResty::Config{'frontend.bulk_insert_limit'};
+    $OpenResty::Limits::COLUMN_LIMIT = to_num
+    $OpenResty::Config{'frontend.column_limit'};
+    $OpenResty::Limits::MODEL_LIMIT = to_num
+    $OpenResty::Config{'frontend.model_limit'};
+    $OpenResty::Limits::POST_LEN_LIMIT = to_num
+    $OpenResty::Config{'frontend.post_len_limit'};
 
-    $CGI::Simple::POST_MAX = to_num $OpenResty::Limits::POST_LEN_LIMIT;  # max 100 K posts
+    $CGI::Simple::POST_MAX = to_num $OpenResty::Limits::POST_LEN_LIMIT;
+    # max 100 K posts
     $CGI::Simple::DISABLE_UPLOADS = 1;  # no uploads
 }
 
 1;
+__END__
+
+=head1 NAME
+
+OpenResty::Config - Configure file reader for OpenResty
+
+=head1 SYNOPSIS
+
+    use OpenResty::Config;
+    OpenResty::Config->init;
+    print $OpenResty::Config{'foo.bar'}; # read the config option's value
+
+=head1 DESCRIPTION
+
+This module reads the configure settings from the config files.
+
+The OpenResty server usually loads two config files, i.e., F<openresty.conf> and F<site_openresty.conf>.
+
+The config files use the synatx similar to F<.ini> files native to Win32 systems. The underlying file reader is actually L<Config::Simple>.
+
+The steps of determining the paths searched for these two config files and the merging algorithm of these two files' settings are given below:
+
+=over
+
+=item 1.
+
+Look for the config file F<$FindBin::Bin/../etc/openresty.conf>. If
+it does not exist or is not a file, it tries to look for
+F</etc/openresty.conf>.
+
+=item 2.
+
+Loads the config settings in the config file determined in step 1 to a
+hash, say, Hash A.
+
+=item 3.
+
+Look for the config file F<$FindBin::Bin/../etc/site_openresty.conf>. If
+it does not exist or is not a file, it tries to look for
+F</etc/site_openresty.conf>.
+
+=item 4.
+
+Loads the config settings in the config file determined in step 3 to a
+hash, say, Hash B.
+
+=item 5.
+
+Merge Hash A obtained in step 2 and Hash B in step 4 using
+L<Hash::Merge>. The settings in F<site_openresty.conf> takes the priority
+over the same setting in F<openresty.conf>.
+
+If an option setting is
+completely missing in F<site_openresty.conf>, then the setting for the same option in F<openresty.conf> (if any) will be used. Note, however, fall-back won't happen when F<site_openresty> has an option with an empty string value, as in
+
+    [frontend]
+    ...
+    filtered=
+
+So in order to use the C<frontend.filtered> setting in the F<openresty.conf> file, one has to remove the whole line altegether or just comment it out like this:
+
+    [frontend]
+    ...
+    #filtered=
+
+=back
+
+=head1 AUTHOR
+
+Agent Zhang (agentzh) C<< <agentzh@yahoo.cn >>.
+
+=head1 SEE ALSO
+
+L<OpenResty>.
 
