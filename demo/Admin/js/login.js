54d22a7b4309024ce5fd6d09b1a8bf1debf7f7c7
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-07-17 09:24:11 +0000

    r10444@agentz-office:  agentz | 2008-07-17 17:19:18 +0800  added an onerror handler to openresty's get method and now it croaks on failed HTTP requests like 404 and 503 :)
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1738 b2c27d59-8999-dc11-853b-000bcdefe56b

--- demo/Admin/js/login.js
+++ demo/Admin/js/login.js
@@ -5,6 +5,9 @@ var userCookie    = 'admin_user';
 var savedAnchor = null;
 var openresty = null;
 
+var waitMessage = null;
+var loadingCount = 0;
+
 $(document).ready(init);
 
 function init () {
@@ -16,6 +19,11 @@ function init () {
         alert('For now, please write to "agentzh" <agentzh@yahoo.cn> to request one :)');
     } );
     $("#login-button").click(login);
+    waitMessage = document.getElementById('wait-message');
+    if (jQuery.browser.opera)
+        $(waitMessage).css('top', '-200px');
+    else
+        $(waitMessage).hide();
 }
 
 function gotoMainPage () {
@@ -33,11 +41,38 @@ function debug (msg) {
     $("#main").append(msg + "<br/>");
 }
 
+function setStatus (isLoading, category) {
+    //debug("set status: " + category + " => " + isLoading);
+    if (isLoading) {
+        if (++loadingCount == 1) {
+            if (jQuery.browser.opera)
+                $(waitMessage).css('top', '2px');
+            else
+                $(waitMessage).show();
+        }
+    } else {
+        loadingCount--;
+        if (loadingCount < 0) loadingCount = 0;
+        if (loadingCount == 0) {
+            // the reason we use this hack is to work around
+            // a rendering bug in Win32 build of Opera
+            // (at least 9.25 and 9.26)
+            if (jQuery.browser.opera)
+                $(waitMessage).css('top', '-200px');
+            else
+                $(waitMessage).hide();
+
+        }
+    }
+    //count++;
+    //debug("[" + count + "] setStatus: " + cat + ": " + loadingCount + "(" + isLoading + ")");
+}
 
 function login () {
     var server = $("#login-server").val();
     var user = $("#login-user").val();
     var password = $("#login-password").val();
+    loadingCount = 0;
     if (!server) {
         error("No server specified.");
         return false;
@@ -47,13 +82,21 @@ function login () {
         return false;
     }
     openresty = new OpenResty.Client( { server: server } );
+
+    setStatus(true, 'login');
     openresty.callback = afterLogin;
+    openresty.onerror = function () {
+        alert("Failed to login.\n  Is your server \"" +
+                server + "\" correct?\n");
+        setStatus(false, 'login');
+    };
     openresty.login(user, password);
     return false;
 }
 
 function afterLogin (res) {
     //alert("After login!");
+    setStatus(false, 'login');
     if (!openresty.isSuccess(res)) {
         error("Failed to login: " + res.error);
         return;
