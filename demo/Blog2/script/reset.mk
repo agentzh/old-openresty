5bf76123ed103e77dec8466cf957a380759ab5d9
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-07-29 03:27:16 +0000

    r10677@agentz-office:  agentz | 2008-07-29 11:21:54 +0800  updated 19-view.t
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1791 b2c27d59-8999-dc11-853b-000bcdefe56b

--- demo/Blog2/script/reset.mk
+++ demo/Blog2/script/reset.mk
@@ -10,6 +10,10 @@ export:
 
 import:
 	script/init.pl -u $(user) -p $(password) -s $(server)
-	../../bin/import-model.pl --reset --step 2 --user $(user) --password $(password) --model Post --server $(server) Post.json
+	../../bin/import-model.pl --reset --step 1 --user $(user) --password $(password) --model Post --server $(server) Post.json
 	../../bin/import-model.pl --reset --user $(user) --password $(password) --model Comment --server $(server) Comment.json
+
+# create or replace function to_fti(text, text) returns tsvector as $$ select setweight(to_tsvector('chinesecfg', $1), 'A') || to_tsvector('chinesecfg', $2) $$ language sql immutable;
+# select title, ts_headline('chinesecfg', content, q) as headline from "Post", to_tsquery('chinesecfg', 'OpenResty|Google') as q where to_fti(title, content) @@ q order by ts_rank(to_fti(title, content), q) desc;
+#
 
