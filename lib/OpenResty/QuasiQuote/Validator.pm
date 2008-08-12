1e0dd3e6e33e682a046fabe3f3468c70be33f70e
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-08-12 11:54:57 +0000

    r10877@agentz-office:  agentz | 2008-08-12 19:55:17 +0800  validator: STRING, INT, IDENT now work
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1844 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty/QuasiQuote/Validator.pm
+++ lib/OpenResty/QuasiQuote/Validator.pm
@@ -21,20 +21,20 @@ value: hash
 hash: '{' <commit> pair(s?) '}' attr(s?)
     {
         my $pairs = $item[3];
-        my $code = <<'_EOC_' . join('', @$pairs);
-ref $_ && ref $_ eq 'HASH' or die "Invalid value",
-    ($_topic ? " for \"$_topic\"" : ""), ": Hash expected.\n";
+        my $topic = $arg{topic};
+        my $for_topic = $topic ? " for $topic" : "";
+        my $code = <<"_EOC_" . join('', @$pairs);
+ref \$_ && ref \$_ eq 'HASH' or die "Invalid value$for_topic: Hash expected.\\n";
 _EOC_
         $return =  $code;
     }
 
-pair: key <commit> ':' value
+pair: key <commit> ':' value[ topic => $item[1] ]
         {
             my $quoted_key = quotemeta($item[1]);
             $return = <<"_EOC_" . $item[4] . "}\n";
 {
 local \$_ = "$quoted_key";
-my \$_topic = "$quoted_key";
 _EOC_
         }
 
@@ -51,13 +51,29 @@ array: '[' <commit> array_elem(s?) ']'
 array_elem: value
 
 type: 'STRING'
-        { <<'_EOC_';
-_STRING($_) or die "Bad value",
-    ($_topic ? " for \"$_topic\"" : ""), ": String expected.\n";
+        {
+            my $topic = $arg{topic};
+            my $for_topic = $topic ? " for $topic" : "";
+            <<"_EOC_";
+defined \$_ and !ref \$_ and length(\$_) or die "Bad value$for_topic: String expected.\\n";
 _EOC_
         }
-    | 'NUM'
+    | 'INT'
+        {
+            my $topic = $arg{topic};
+            my $for_topic = $topic ? " for $topic" : "";
+            <<"_EOC_";
+defined \$_ and /^[-+]?\\d+\$/ or die "Bad value$for_topic: Integer expected.\\n";
+_EOC_
+        }
     | 'IDENT'
+        {
+            my $topic = $arg{topic};
+            my $for_topic = $topic ? " for $topic" : "";
+            <<"_EOC_";
+defined \$_ and /^\\w+\$/ or die "Bad value$for_topic: Identifier expected.\\n";
+_EOC_
+        }
 
 attr: ':' <commit> ident arguments(?)
 
