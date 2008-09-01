ffcd8c3fd5a16fb9f12ef6e9a1e05497ef08ec29
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-09-01 10:41:25 +0000

    r11399@agentz-office:  agentz | 2008-09-01 18:41:40 +0800  now restore-metamodel.pl and save-metamodel.pl work with PgFarm as well :)
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@1971 b2c27d59-8999-dc11-853b-000bcdefe56b

--- bin/restore-metamodel.pl
+++ bin/restore-metamodel.pl
@@ -14,11 +14,13 @@ eval {
 };
 warn $@ if $@;
 
+my $host_suffix = shift || '';
+
 #my $dump_file = 'metamodel.sql';
 my $backend = $OpenResty::Backend;
 my $backend_name = $OpenResty::BackendName;
 
-my @files = map glob, (@ARGV || 'metamodel/*.sql');
+my @files = glob 'metamodel/*.sql';
 my @accounts;
 for my $file (@files) {
     if ($file =~ /(\w+).sql/) {
@@ -32,61 +34,74 @@ for my $file (@files) {
 #my @tables = qw(
 #_views _models _columns _feeds _roles _access _general
 #);
-if ($backend_name eq 'Pg') {
-    my $db = $OpenResty::Config{'backend.database'};
-    my $user = $OpenResty::Config{'backend.user'};
-    my $password = $OpenResty::Config{'backend.password'};
+my $db = $OpenResty::Config{'backend.database'};
+my $user = $OpenResty::Config{'backend.user'};
+my $password = $OpenResty::Config{'backend.password'};
+my $host = 'localhost';
 
-    for my $account (@accounts) {
-        my $sql_file = shift @files;
-        my $sql = <<'_EOC_';
+for my $account (@accounts) {
+    if (!$backend->has_user($account)) {
+        if ($backend_name eq 'Pg') {
+            warn "Adding account $account...\n";
+            $backend->do("create schema $account;");
+        } else {
+            die "User $account not found.\n";
+        }
+    }
+    $backend->set_user($account);
+
+    my $machine = $backend->has_user($account);
+    if ($backend_name eq 'PgFarm') {
+        $db = $machine;
+        $host = $machine . $host_suffix;
+    }
+
+    my $sql_file = shift @files;
+    my $sql = <<'_EOC_';
 SELECT
-  c.relname, c.relkind
+c.relname, c.relkind
 FROM pg_catalog.pg_class c
-     JOIN pg_catalog.pg_roles r ON r.oid = c.relowner
-     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+    JOIN pg_catalog.pg_roles r ON r.oid = c.relowner
+    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
 WHERE n.nspname NOT IN ('pg_catalog', 'pg_toast')
-  AND pg_catalog.pg_table_is_visible(c.oid)
-  AND c.relname ~ '^_'
+AND pg_catalog.pg_table_is_visible(c.oid)
+AND c.relname ~ '^_'
 ORDER BY c.relkind DESC
 _EOC_
-        $backend->set_user($account);
-        my $res;
-        eval {
-            $res = $backend->select($sql);
-        };
-        if ($@) { warn $@; }
-        #my @rels = map { $_->[0] } @$res;
-        ### $res
-        for my $rel (@$res) {
-            ### $rel
-            my ($name, $kind) = @$rel;
-            if ($kind eq 'r') {
-                eval {
-                    $backend->do("drop table if exists \"$name\" cascade;");
-                };
-                if ($@) { warn $@ }
-            } elsif ($kind eq 'S') {
-                eval {
-                    $backend->do("drop sequence if exists \"$name\" cascade;");
-                };
-                if ($@) { warn $@ }
-            } elsif ($kind eq 'i') {
-                eval {
-                    $backend->do("drop index if exists \"$name\" cascade;");
-                };
-                if ($@) { warn $@ }
-            } else {
-                die "Ignoring database object $name of kind $kind\n";
-            }
+    my $res;
+    eval {
+        $res = $backend->select($sql);
+    };
+    if ($@) { warn $@; }
+    #my @rels = map { $_->[0] } @$res;
+    ### $res
+    for my $rel (@$res) {
+        ### $rel
+        my ($name, $kind) = @$rel;
+        if ($kind eq 'r') {
+            eval {
+                $backend->do("drop table if exists \"$name\" cascade;");
+            };
+            if ($@) { warn $@ }
+        } elsif ($kind eq 'S') {
+            eval {
+                $backend->do("drop sequence if exists \"$name\" cascade;");
+            };
+            if ($@) { warn $@ }
+        } elsif ($kind eq 'i') {
+            eval {
+                $backend->do("drop index if exists \"$name\" cascade;");
+            };
+            if ($@) { warn $@ }
+        } else {
+            die "Ignoring database object $name of kind $kind\n";
         }
-        warn "Importing metamodel for account $account from $sql_file...\n";
-        if (system("psql -U $user -qn -d $db -f $sql_file > /dev/null") != 0) {
-            warn "Failed to import metamodel for account $account from $sql_file.\n";
-        } else { warn "Done.\n"; }
-
-        #die;
     }
+    warn "Importing metamodel for account $account from $sql_file...\n";
+    if (system("psql -U $user -qn -d $db -f $sql_file > /dev/null") != 0) {
+        warn "Failed to import metamodel for account $account from $sql_file.\n";
+    } #else { warn "Done.\n"; }
+
     #die;
 }
 
--- bin/save-metamodel.pl
+++ bin/save-metamodel.pl
@@ -15,6 +15,7 @@ eval {
 warn $@ if $@;
 
 #my $dump_file = 'metamodel.sql';
+my $host_suffix = shift || '';
 my $backend = $OpenResty::Backend;
 my $backend_name = $OpenResty::BackendName;
 
@@ -24,42 +25,46 @@ my @accounts = $backend->get_all_accounts;
 #my @tables = qw(
 #_views _models _columns _feeds _roles _access _general
 #);
-if ($backend_name eq 'Pg') {
-    if ( ! -d 'metamodel' ) { mkdir 'metamodel' }
-    #system("mv $dump_file $dump_file.old");
-    my $db = $OpenResty::Config{'backend.database'};
-    my $user = $OpenResty::Config{'backend.user'};
-    my $password = $OpenResty::Config{'backend.password'};
+if ( ! -d 'metamodel' ) { mkdir 'metamodel' }
+#system("mv $dump_file $dump_file.old");
+my $db = $OpenResty::Config{'backend.database'};
+my $user = $OpenResty::Config{'backend.user'};
+my $password = $OpenResty::Config{'backend.password'};
+my $host = 'localhost';
 
-    for my $account ('_global', @accounts) {
-        my $sql = <<'_EOC_';
-SELECT
-  c.relname
-FROM pg_catalog.pg_class c
-     JOIN pg_catalog.pg_roles r ON r.oid = c.relowner
-     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-WHERE c.relkind = 'r'
-      AND n.nspname NOT IN ('pg_catalog', 'pg_toast')
-  AND pg_catalog.pg_table_is_visible(c.oid)
-  AND c.relname ~ '^_'
-_EOC_
-        $backend->set_user($account);
-        my $res;
-        eval {
-            $res = $backend->select($sql);
-        };
-        if ($@) { warn $@; next }
-        my @tables = map { $_->[0] } @$res;
-        ### @tables
-        #else {dump_res($res);
-        my $tables = join ' ', map { "-t '$account.$_'" } @tables;
-        my $cmd = "pg_dump -U $user -x $tables -f metamodel/$account.sql $db";
-        #warn $cmd;
-        warn "Dumping account $account to metamodel/$account.sql...\n";
-        if (system($cmd) != 0) {
-            warn "Failed to dump metamodel from $account\n";
-        }
-        #system("cat tmp.sql >> $dump_file");
+for my $account ('_global', @accounts) {
+    $backend->set_user($account);
+    my $machine = $backend->has_user($account);
+    if ($backend_name eq 'PgFarm') {
+        $db = $machine;
+        $host = $machine . $host_suffix;
     }
+    my $sql = <<'_EOC_';
+SELECT
+c.relname
+FROM pg_catalog.pg_class c
+    JOIN pg_catalog.pg_roles r ON r.oid = c.relowner
+    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE c.relkind = 'r'
+    AND n.nspname NOT IN ('pg_catalog', 'pg_toast')
+AND pg_catalog.pg_table_is_visible(c.oid)
+AND c.relname ~ '^_'
+_EOC_
+    my $res;
+    eval {
+        $res = $backend->select($sql);
+    };
+    if ($@) { warn "select pg_catalog failed!!!\n"; warn $@; next }
+    my @tables = map { $_->[0] } @$res;
+    ### @tables
+    #else {dump_res($res);
+    my $tables = join ' ', map { "-t '$account.$_'" } @tables;
+    my $cmd = "pg_dump -iOx -h $host -U $user $tables -f metamodel/$account.sql $db";
+    #warn $cmd;
+    warn "Dumping account $account to metamodel/$account.sql...\n";
+    if (system($cmd) != 0) {
+        warn "Failed to dump metamodel from $account\n";
+    }
+    #system("cat tmp.sql >> $dump_file");
 }
 
