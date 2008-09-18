8e8b60c9c14f6bf5c23c5bcb97f5bd6c4b201341
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-09-18 02:19:09 +0000

    added bin/httpresty.pl script
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2020 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ bin/httpresty.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib '../../lib';
+use utf8;
+use JSON::XS;
+use YAML 'Dump';
+use WWW::OpenResty::Simple;
+#use Date::Manip;
+use Getopt::Std;
+
+#$JSON::Syck::ImplicitUnicode = 1;
+#$YAML::Syck::ImplicitUnicode = 1;
+my $json_xs = JSON::XS->new->utf8->allow_nonref;
+
+my %opts;
+getopts('u:s:p:h', \%opts);
+if ($opts{h}) {
+    die "Usage: $0 -u <user> -p <password> -s <openresty_server>\n";
+}
+my $user = $opts{u} or
+    die "No OpenResty account name specified via option -u\n";
+my $password = $opts{p} or
+    die "No OpenResty account's Admin password specified via option -p\n";
+my $server = $opts{s} || 'http://api.openresty.org';
+
+my $resty = WWW::OpenResty::Simple->new( { server => $server } );
+$resty->login($user, $password);
+
+my $meth = shift or
+    die "No method specified.\n";
+$meth = lc($meth);
+
+my $url = shift or
+    die "No url spcified.\n";
+
+if (!$resty->can($meth)) {
+    die "Method $meth not found.\n";
+}
+
+my $res = $resty->$meth($url);
+print $json_xs->encode($res), "\n";
+
