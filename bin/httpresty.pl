71f6ef162115956a1345659360595e3953db1ab9
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-09-18 03:06:37 +0000

    fixed charset and output
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2022 b2c27d59-8999-dc11-853b-000bcdefe56b

--- bin/httpresty.pl
+++ bin/httpresty.pl
@@ -10,15 +10,16 @@ use YAML 'Dump';
 use WWW::OpenResty::Simple;
 #use Date::Manip;
 use Getopt::Std;
+use Encode qw(from_to);
 
 #$JSON::Syck::ImplicitUnicode = 1;
 #$YAML::Syck::ImplicitUnicode = 1;
 my $json_xs = JSON::XS->new->utf8->allow_nonref;
 
 my %opts;
-getopts('u:s:p:h', \%opts);
+getopts('u:s:p:c:h', \%opts);
 if ($opts{h}) {
-    die "Usage: $0 -u <user> -p <password> -s <openresty_server> <http_method> <url>\n";
+    die "Usage: $0 -c <charset> -u <user> -p <password> -s <openresty_server> <http_method> <url>\n";
 }
 my $user = $opts{u} or
     die "No OpenResty account name specified via option -u\n";
@@ -35,11 +36,18 @@ $meth = lc($meth);
 
 my $url = shift or
     die "No url spcified.\n";
+if ($opts{c}) {
+    from_to($url, $opts{c}, 'utf8');
+}
 
 if (!$resty->can($meth)) {
     die "Method $meth not found.\n";
 }
 
 my $res = $resty->$meth($url);
-print $json_xs->encode($res), "\n";
+my $out = Dump($res);
+if ($opts{c}) {
+    from_to($out, 'utf8', $opts{c});
+}
+print $out, "\n";
 
