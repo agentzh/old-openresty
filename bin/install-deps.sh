087d76eb2211b80378aaf14283b4136d061850af
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-10-13 10:19:14 +0000

    added bin/install-deps.sh
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2106 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ bin/install-deps.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+#sudo cpan -f Term::ReadLine::Gnu
+sudo cpan YAML GD CGI::Cookie::XS FCGI CGI::Simple YAML::Syck List::Util \
+    DBI Clone Params::Util Parse::Yapp DBD::Pg Data::UUID \
+    GD::SecurityImage Cache::Memcached::Fast Hash::Merge \
+    Config::Simple Encode::Guess List::MoreUtils JSON::XS \
+    Data::Structure::Util DateTime::Format::Pg \
+    DateTime::Format::Strptime Digest::MD5 File::ShareDir \
+    Compress::Zlib Crypt::CBC Crypt::Rijndael IPC::Run3 IPC::Run \
+    Time::HiRes Filter::QuasiQuote Parse::RecDescent Benchmark::Timer \
+    HTTP::Server::Simple Text::Diff Text::Table \
+    Term::ReadLine::Perl Term::ReadLine::Gnu Term::ReadKey \
+    CGI Cache::Cache Class::Prototyped Test::LongString \
+    HTTP::Response WWW::OpenResty LWP::UserAgent
+
