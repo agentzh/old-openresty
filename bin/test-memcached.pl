026ce7f9615b9f456f9d4f186fd10b7b615dc30e
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-11-07 03:23:39 +0000

    added test-memcached.pl script
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2186 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ bin/test-memcached.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Test::More;
+
+use Cache::Memcached::Fast;
+my @servers = map glob, @ARGV;
+if (!@servers) { die "No server specified.\n"; }
+
+plan tests => 2 * @servers;
+
+for my $server (@servers) {
+    my $obj = Cache::Memcached::Fast->new({
+        servers => [$server],
+    });
+
+    ok $obj, "$server - memcached obj ok";
+    $obj->delete('foo');
+    $obj->set(foo => $server, 3);
+    my $value = $obj->get('foo');
+    is $value, $server, "$server - successfully set and get the key foo";
+}
+
