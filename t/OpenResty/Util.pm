1ff4dd5104b40a751b33805848cc87b73f3fed13
clkao <clkao@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-11-09 04:53:06 +0000

    - 00-prereq to bail out if we fail basic test, most likely due to   incorrect db config. - clear cache for local tests. - auto create test accounts for local tests.
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2191 b2c27d59-8999-dc11-853b-000bcdefe56b

---
+++ t/OpenResty/Util.pm
@@ -0,0 +1,21 @@
+package t::OpenResty::Util;
+
+# this file contains util that can't be in t::openresty, because
+# spiffy is polluting the args passed in for some unknown reasons
+
+sub ensure_test_user {
+    my ($user, $password) = @_;
+    local $SIG{__WARN__} = sub { }
+        unless $ENV{TEST_VERBOSE};
+    unless ($ENV{OPENRESTY_TEST_SERVER}) {
+        my $backend = $OpenResty::Backend;
+        if ( $backend->has_user($user) ) {
+            $backend->drop_user($user);
+        }
+        $backend->add_user($user, $password);
+        $backend->has_user($user)
+            or BAIL_OUT('Can not create test user');
+    }
+}
+
+1;
