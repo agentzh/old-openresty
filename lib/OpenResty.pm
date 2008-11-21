6489bb5cde36f0395f10ee537dbd6c6dfb733976
agentz <agentz@b2c27d59-8999-dc11-853b-000bcdefe56b>
2008-11-21 05:41:07 +0000

    releng work for v0.5.1
    git-svn-id: http://svn.openfoundry.org/openapi/trunk@2202 b2c27d59-8999-dc11-853b-000bcdefe56b

--- lib/OpenResty.pm
+++ lib/OpenResty.pm
@@ -1,6 +1,6 @@
 package OpenResty;
 
-our $VERSION = '0.005000';
+our $VERSION = '0.005001';
 
 use strict;
 use warnings;
@@ -674,7 +674,7 @@ OpenResty - General-purpose web service platform for web applications
 
 =head1 VERSION
 
-This document describes OpenResty 0.5.0 released on September 27, 2008.
+This document describes OpenResty 0.5.1 released on November 21, 2008.
 
 =head1 DESCRIPTION
 
