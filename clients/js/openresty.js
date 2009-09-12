17e50e5d5425b95bea433e7e7496aecfb548e9cd
agentzh (章亦春) <agentzh@gmail.com>
2009-09-12 21:18:10 +0800

    updated openresty.js to the kindy version :)

--- clients/js/openresty.js
+++ clients/js/openresty.js
@@ -1,6 +1,27 @@
-if (typeof window.OpenResty == "undefined") {
+var console, _log;
+(function(){
+if(typeof console == 'undefined'){
+  _log = function(a, b, c){/*alert('' + a + b + c)*/};
+}else{
+  _log = function(){
+    console.log.apply(console, arguments)
+  };
+}
+})();
 
-window.undefined = window.undefined;
+
+if (typeof window.OpenResty == "undefined") {
+(function() {
+var undefined;
+var browser = (function(){
+    var u = navigator.userAgent.toLowerCase(), a = /(msie|firefox|camino|chrome|opera|safari|netscape|konqueror|lynx)/, b = /(msie|firefox|chrome|opera|safari|camino|netscape|netscape6|konqueror|lynx|version)(\/|\s)([a-z0-9\.\+]*?)(\;|dev|rel|\s|$)/;
+    var un = 'unknown', x = 'X', r = {
+        name: (a.exec(u) || [un, un])[1]
+    };
+    r[r.name] = true;
+    r.version = (b.exec(u) || [x, x, x, x])[3];
+    return r;
+})();
 
 var OpenResty = {
     callbackMap: {},
@@ -20,15 +41,17 @@ OpenResty.Client = function (params) {
     //this.password = params.password;
 };
 
-OpenResty.Client.prototype.isSuccess = function (res) {
+var $ClientP = OpenResty.Client.prototype;
+$ClientP.isSuccess = function (res) {
+    // alert(JSON.stringify(res));
     return !(typeof res == 'object' && res.success == 0 && res.error);
 };
 
-OpenResty.Client.prototype.logout = function () {
+$ClientP.logout = function () {
     this.get('/=/logout');
 };
 
-OpenResty.Client.prototype.login = function (user, password) {
+$ClientP.login = function (user, password) {
     this.user = user;
     var userCallback = this.callback;
     if (typeof userCallback == 'string') {
@@ -49,7 +72,7 @@ OpenResty.Client.prototype.login = function (user, password) {
     this.get('/=/login/' + user + '/' + password);
 };
 
-OpenResty.Client.prototype.postByGet = function (url) {
+$ClientP.postByGet = function (url) {
     var args, content;
     if (arguments.length == 3) {
         args = arguments[1];
@@ -66,16 +89,164 @@ OpenResty.Client.prototype.postByGet = function (url) {
     this.get(url, args);
 };
 
-OpenResty.Client.prototype.genId = function () {
-    return ( Math.random() * 1000000 );
+$ClientP.genId = (function (n) {
+    //return ( Math.random() * 1000000 );
+    return function() { return n++ };
     //return this.counter++;
-}
+})(new Date().getTime());
 
-OpenResty.Client.prototype.post = function (url) {
-    if (jQuery.browser.opera) {  // work around an Opera bug
-        return OpenResty.Client.prototype.postByGet.apply(this, arguments);
+/*
+  post: for large data transform
+    crossdomain ~ y/n
+    
+    _post_by_form({
+        form: form,
+        url: fullURL,
+        content: { data: content },
+        preventCache: true,
+        method: "post",
+        handleAs: 'html',
+        handle: function () {
+            self.get('/=/last/response/' + args._last_response);
+        }
+    });
+*/
+
+
+var _post_forms_div,
+    _post_forms = {};
+
+OpenResty.Client._post_forms = _post_forms;
+
+$ClientP._post_by_form = function (url, content, args) {
+    if (!_post_forms_div) {
+        _post_forms_div = document.createElement('div');
+        var _s = _post_forms_div.style;
+        _s.position = 'absolute';
+        _s.left = '-1000px';
+        _s.width = '500px';
+        _s.height = '1px';
+        _s.overflow = 'hidden';
+        _s = null;
+        document.body.appendChild(_post_forms_div);
+    }
+    
+    var reqId = args._last_response;
+    
+    if (reqId in _post_forms) {
+        throw "the post already exist!!";
+    }
+    
+    var self = this;
+    var obj = {
+        reqId: reqId
+    };
+    _post_forms[reqId] = obj;
+    var submited = false;
+    // var onloadCount = 0;
+    obj.onload = function() {
+        if (!submited) return;
+        
+        // alert('contentLoad ' + (++onloadCount) + ' times\n' + this);
+        
+        var _iframe = obj.iframe;
+        
+        // maybe we can process response under same domain
+        
+        delete obj.onload;
+        
+        _post_forms_div.removeChild(_iframe);
+        var _form = obj.form;
+        _post_forms_div.removeChild(obj.form);
+        
+        _iframe = null;
+        _form = null;
+        
+        delete obj.iframe;
+        delete obj.form;
+        
+        delete _post_forms[reqId];
+        
+        obj = null;
+        
+        self.get('/=/last/response/' + reqId);
+    };
+    
+    var form_id = 'OpenResty_form_' + reqId,
+        iframe_id = 'OpenResty_iframe_' + reqId;
+    
+    var _5 = 'http://ajax.googleapis.com/ajax/libs/dojo/1.3.2/dojo/resources/blank.html';
+    
+    var _iframe;
+    var _6 = browser.msie ? '<iframe name="' + iframe_id + '" src="' + _5 + '" onload="OpenResty.Client._post_forms[' + reqId + '].onload();">': "iframe";
+    
+    _iframe = document.createElement(_6);
+    
+    with(_iframe) {
+        name = iframe_id;
+        setAttribute("name", iframe_id);
+        id = iframe_id;
+    }
+    
+    if (!browser.msie) {
+        _iframe.src = _5;
+        _iframe.onload = OpenResty.Client._post_forms[reqId].onload;
+    }
+    
+    _post_forms_div.appendChild(_iframe);
+    obj.iframe = _iframe;
+    
+    var _form = document.createElement("form");
+    
+    var _22 = _form.getAttributeNode("action");
+    var _23 = _form.getAttributeNode("method");
+    var _24 = _form.getAttributeNode("target");
+
+    if (_22) {
+        _22.value = url;
+    } else {
+        _form.setAttribute("action", url);
     }
 
+    if (_23) {
+        _23.value = "post";
+    } else {
+        _form.setAttribute("method", "post");
+    }
+    
+    if (_24) {
+        _24.value = iframe_id;
+    } else {
+        _form.setAttribute("target", iframe_id);
+    }
+    
+    _post_forms_div.appendChild(_form);
+    obj.form = _form;
+    
+    var _ipt;
+    if (browser.msie) {
+        _ipt = document.createElement('<input type="hidden" name="data">');
+    } else {
+        _ipt = document.createElement("input");
+        _ipt.name = "data";
+        _ipt.type = "hidden";
+    }
+    _ipt.value = content;
+    _form.appendChild(_ipt);
+    
+    _ipt = null;
+    
+    _form.submit();
+    submited = true;
+};
+
+$ClientP.post = function (url) {
+/*
+    if (browser.opera) {  // work around an Opera bug
+        return $ClientP.postByGet.apply(this, arguments);
+    }*/
+
+    try{
     var args, content;
     if (arguments.length == 3) {
         args = arguments[1];
@@ -87,8 +258,9 @@ OpenResty.Client.prototype.post = function (url) {
     //url = url.replace(/^\/=\//, '/=/post/');
     //if (url.match(/\?/)) throw "URL should not contain '?'.";
     if (!this.callback) throw "No callback specified for OpenResty.";
-    var formId = this.formId;
-    if (!formId) throw "No form specified.";
+    
+    // var formId = this.formId;
+    // if (!formId) throw "No form specified.";
 
     if (this.session) args._session = this.session;
     if (!this.session && !args._user)
@@ -116,26 +288,14 @@ OpenResty.Client.prototype.post = function (url) {
         fullURL += '?' + arg_list.join("&");
 
     var self = this;
-    //if (jQuery.browser.opera) {  // work around an Opera bug
+    //if (browser.opera) {  // work around an Opera bug
         //$("#" + formId).html($("#" + formId).html());
     //}
-    var form = document.getElementById(formId);
-    form.method = 'POST';
+    // var form = document.getElementById(formId);
+    // form.method = 'POST';
 
-    var ts = dojo.io.iframe.send({
-        form: form,
-        url: fullURL,
-        content: { data: content },
-        preventCache: true,
-        method: "post",
-        handleAs: 'html',
-        handle: function () {
-            //alert("Getting last response!");
-            //alert(args._last_response);
-            self.get('/=/last/response/' + args._last_response);
-        }
-    });
-
+    this._post_by_form(fullURL, content, args);
+    }catch(ex){alert('error!!!: ' + ex)}
     /*
     var form = document.getElementById('new_model');
     form.action = this.server + url;
@@ -148,7 +308,7 @@ OpenResty.Client.prototype.post = function (url) {
     //
 };
 
-OpenResty.Client.prototype.putByGet = function (url) {
+$ClientP.putByGet = function (url) {
     var args, content;
     if (arguments.length == 3) {
         args = arguments[1];
@@ -165,9 +325,9 @@ OpenResty.Client.prototype.putByGet = function (url) {
     this.get(url, args);
 };
 
-OpenResty.Client.prototype.put = function (url) {
-    if (jQuery.browser.opera) {  // work around an Opera bug
-        return OpenResty.Client.prototype.putByGet.apply(this, arguments);
+$ClientP.put = function (url) {
+    if (browser.opera) {  // work around an Opera bug
+        return $ClientP.putByGet.apply(this, arguments);
     }
 
     var args, content;
@@ -185,7 +345,7 @@ OpenResty.Client.prototype.put = function (url) {
 };
 
 
-OpenResty.Client.prototype.get = function (url, args) {
+$ClientP.get = function (url, args) {
     if (!args) args = {};
     if (!this.callback) throw "No callback specified for OpenResty.";
     if (!this.server) throw "No server specified for OpenResty.";
@@ -200,10 +360,8 @@ OpenResty.Client.prototype.get = function (url, args) {
     var reqId = this.genId();
     //args._rand = reqId;
 
-    var onerror = this.onerror;
-    if (onerror == null)
-        onerror = function () { alert("Failed to do GET " + url) };
-
+    var onerror = this.onerror || function () { alert("Failed to do GET " + url) };
+    
     //alert(args._rand);
     //if (!isLogin) args.user = this.user;
     //args.password = this.password;
@@ -275,13 +433,13 @@ OpenResty.Client.prototype.get = function (url, args) {
     }, 0);
 };
 
-OpenResty.Client.prototype.del = function (url, args) {
+$ClientP.del = function (url, args) {
     if (!args) args = {};
     url = url.replace(/^\/=\//, '/=/delete/');
     this.get(url, args);
 };
 
-OpenResty.Client.prototype.purge = function () {
+$ClientP.purge = function () {
     // document.getElementByClassName('openapiScriptTag').remove();
     OpenResty.callbackMap = {};
     OpenResty.isDone = {};
@@ -294,5 +452,6 @@ OpenResty.Client.prototype.purge = function () {
     }
 };
 
+window.OpenResty = OpenResty;
+})();
 }
-
